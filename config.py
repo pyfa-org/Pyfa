9aed19ec4519d5236ef09de8677b3cc2b55fb363
cncfanatics <diego.duclos@gmail.com>
2010-08-19 16:39:51 +0200

    Add a debug flag, currently unused

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 import os.path
 import sys
 
+debug = False
+
 #Path autodetection, only change if it doesn't work
 path = os.path.dirname(unicode(__file__, sys.getfilesystemencoding()))
 homePath = os.path.expanduser(os.path.join("~", ".pyfa"))
+
