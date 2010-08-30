a3f66cd5f8fe673ffd956c9d9c033499920ddf47
cncfanatics <diego.duclos@gmail.com>
2010-08-30 12:22:03 +0200

    Add a check for fit is None

--- gui/statsPane.py
+++ gui/statsPane.py
@@ -177,7 +177,7 @@ class StatsPane(wx.Panel):
                 else:
                     lbl.SetLabel("0.0")
 
-        tank = fit.effectiveTank
+        tank = fit.effectiveTank if fit != None else None
         maxTank = ("shieldPassive", fit.calculateShieldRecharge() if fit else 0)
         for tankType in ("shield", "armor", "hull"):
             if tank is not None:
