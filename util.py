131e603fea06feebd82d3d83bc1824a35d852765
cncfanatics <diego.duclos@gmail.com>
2010-09-03 12:05:01 +0200

    shorten stuff more agressivly

--- util.py
+++ util.py
@@ -1,9 +1,9 @@
 def shorten(val, digits):
-    if val > 10**10:
+    if val > 10**8:
         return ("%." + str(digits)+ "fG") % (val / float(10**9))
-    elif val > 10**7:
+    elif val > 10**6:
         return ("%." + str(digits)+ "fM") % (val / float(10**6))
-    elif val > 10**4:
+    elif val > 10**3:
         return ("%." + str(digits)+ "fk") % (val / float(10**3))
     else:
         return ("%." + str(digits) + "f") % val
