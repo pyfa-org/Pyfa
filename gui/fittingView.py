2fe3b8271deb8a12a951f70684d9860a8ae807b8
HomeWorld <homeworld@gmail.com>
2010-09-15 17:01:28 +0300

    Revert "Don't reclear and repopulate the whole fitting view whenever anything"
    This reverts commit 35d9a42d70b5706018a94ab36ab789d83da1a575.

--- gui/fittingView.py
+++ gui/fittingView.py
@@ -42,7 +42,6 @@ class FittingView(d.Display):
         self.Bind(wx.EVT_LEFT_DCLICK, self.removeItem)
         self.Hide() #Don't show ourselves at start
         self.activeFitID = None
-        self.ignore = False
 
     #Gets called from the fitMultiSwitch when it decides its time
     def changeFit(self, fitID):
@@ -52,20 +51,14 @@ class FittingView(d.Display):
         else:
             self.Show()
 
-        self.ignore = True
         wx.PostEvent(self.mainFrame, FitChanged(fitID=fitID))
-        self.ignore = False
-        self.slotsChanged()
 
     def appendItem(self, itemID):
         fitID = self.activeFitID
         if fitID != None:
             cFit = controller.Fit.getInstance()
             cFit.appendModule(fitID, itemID)
-            self.ignore = True
             wx.PostEvent(self.mainFrame, FitChanged(fitID=fitID))
-            self.ignore = False
-            self.slotsChanged()
 
     def removeItem(self, event):
         row, _ = self.HitTest(event.Position)
@@ -74,14 +67,11 @@ class FittingView(d.Display):
             trigger = cFit.removeModule(self.activeFitID, self.mods[self.GetItemData(row)].position)
 
             if trigger:
-                self.ignore = True
                 wx.PostEvent(self.mainFrame, FitChanged(fitID=self.activeFitID))
-                self.ignore = False
-                self.slotsChanged()
 
-    def generateMods(self):
+    def fitChanged(self, event):
         cFit = controller.Fit.getInstance()
-        fit = cFit.getFit(self.activeFitID)
+        fit = cFit.getFit(event.fitID)
 
         slotOrder = [Slot.SUBSYSTEM, Slot.HIGH, Slot.MED, Slot.LOW, Slot.RIG]
 
@@ -91,14 +81,6 @@ class FittingView(d.Display):
         else:
             self.mods = None
 
-    def slotsChanged(self):
-        self.generateMods()
         self.populate(self.mods)
 
-    def fitChanged(self, event):
-        if self.ignore:
-            return
-
-        self.generateMods()
-        self.refresh(self.mods)
         event.Skip()
