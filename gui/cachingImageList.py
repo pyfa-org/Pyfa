a69a601666e6a464df98f6a3bf7155686fea3a10
cncfanatics <diego.duclos@gmail.com>
2010-10-05 19:08:44 +0200

    Add a CachingImageList

---
+++ gui/cachingImageList.py
@@ -0,0 +1,30 @@
+#===============================================================================
+# Copyright (C) 2010 Diego Duclos
+#
+# This file is part of pyfa.
+#
+# pyfa is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# pyfa is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with pyfa.  If not, see <http://www.gnu.org/licenses/>.
+#===============================================================================
+
+import wx
+
+class CachingImageList(wx.ImageList):
+    map = {}
+    def Add(self, bitmap):
+        id = self.map.get(id)
+        if id is None:
+            id = wx.ImageList.Add(bitmap)
+            self.map[id] = bitmap
+
+        return id
