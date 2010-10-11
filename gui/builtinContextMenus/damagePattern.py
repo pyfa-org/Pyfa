58dfb8ba851935dc1d67de7df17ca730271ece87
cncfanatics <diego.duclos@gmail.com>
2010-10-11 10:22:26 +0200

    Add "Raw" damage pattern, which'll display raw health without resistance

--- gui/builtinContextMenus/damagePattern.py
+++ gui/builtinContextMenus/damagePattern.py
@@ -17,13 +17,15 @@ class DamagePattern(ContextMenu):
         sDP = service.DamagePattern.getInstance()
         self.patterns = sDP.getDamagePatternList()
         self.patterns.sort(key=lambda p: p.name)
-        return map(lambda p: p.name, self.patterns)
+        m = map(lambda p: p.name, self.patterns)
+        m.append("Raw")
+        return m
 
     def activate(self, context, selection, i):
         sDP = service.DamagePattern.getInstance()
         sFit = service.Fit.getInstance()
         fitID = self.mainFrame.getActiveFit()
-        sFit.setDamagePattern(fitID, self.patterns[i])
+        sFit.setDamagePattern(fitID, self.patterns[i] if i < len(self.patterns) else None)
         wx.PostEvent(self.mainFrame, gui.fittingView.FitChanged(fitID=fitID))
 
 DamagePattern.register()
