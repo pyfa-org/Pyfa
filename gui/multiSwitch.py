0c7d25bffb7aa11f082e7e7f30ce60ba5c12156b
HomeWorld <homeworld@gmail.com>
2010-10-16 16:08:15 +0300

    Revert "Fix attempt for a traceback caused by removeTab... imglist Remove() on wxmac"
    This reverts commit f309597cda9eda0bd201bc807d14f6cc1757ea71.

--- gui/multiSwitch.py
+++ gui/multiSwitch.py
@@ -88,8 +88,7 @@ class MultiSwitch(wx.Notebook):
             self.setTabTitle(i, None)
             remIcon = self.GetPageImage(i)
             self.SetPageImage(i, -1)
-            if remIcon != -1:
-                self.ImageList.Remove(remIcon)
+            self.ImageList.Remove(remIcon)
             page = self.GetPage(i)
             if page.type == "fit":
                 page.view.changeFit(None)
