ce3ae7ecb9bfd9a879ee0142abd77538817e3151
HomeWorld <homeworld@gmail.com>
2010-10-27 16:28:09 +0300

    Revert "Fix attempt for wxgtk overzealousness"
    This reverts commit e5fc402565cb64f4d5c72aa34a55fcfe3711a2de.

--- gui/shipBrowser.py
+++ gui/shipBrowser.py
@@ -556,12 +556,8 @@ class CategoryItem(wx.Window):
         wx.PostEvent(self.shipBrowser,Stage2Selected(categoryID=categoryID))
 
     def enterW(self,event):
-        mposx,mposy = self.Parent.ScreenToClient(wx.GetMousePosition())
-        iposx,iposy = self.GetPosition()
-        iwidth, iheight = self.GetSize()
-        if mposy >= iposy and mposy<iposy+iheight:
-            self.highlighted = 1
-            self.Refresh()
+        self.highlighted = 1
+        self.Refresh()
         event.Skip()
 
     def leaveW(self,event):
