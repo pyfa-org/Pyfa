ccd13e1b20cdb5e4ac6d1a24ee1d8ae6eafb028a
Corollax <corollax@gmail.com>
2010-11-02 11:46:52 -0500

    Fix a spacing issue in __init__.py
    No biggie.

--- gui/builtinViewColumns/__init__.py
+++ gui/builtinViewColumns/__init__.py
@@ -1,5 +1,5 @@
 __all__ = ["moduleState", "moduleName", "attributeDisplay", "maxRange",
            "name", "droneDps", "droneNameAmount", "droneCheckbox", "moduleAmmo",
            "capacitorUse", "activityCheckbox", "moduleAmmoIcon", "modulePrice",
-           "projectedName", "projectedState", "projectedAmmoIcon", "projectedAmmo",
-           "moduleTracking"]
+           "projectedName", "projectedState", "projectedAmmoIcon", 
+           "projectedAmmo", "moduleTracking"]
