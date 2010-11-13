30c92d0808b81ff483cb90685fcec8e34f8e926c
HomeWorld <homeworld@gmail.com>
2010-11-14 00:00:30 +0200

    Debug msg for tab rect

--- miniframe.py
+++ miniframe.py
@@ -254,6 +254,8 @@ class PFTabRenderer:
         r.top = r.left = 0
         r.height = height
 
+        print "Tab rect", r
+
         mdc.GradientFillLinear(r,self.gradientStartColor,self.tabColor,wx.SOUTH)
         mdc.SetPen( wx.Pen(self.leftColor, width = 1 ) )
 
