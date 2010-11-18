778d4e30cb6d2409fa80f1e1101271e6b540d1f5
HomeWorld <homeworld@gmail.com>
2010-11-18 11:43:36 +0200

    Fixed an oopsie

--- gui/chromeTabs.py
+++ gui/chromeTabs.py
@@ -676,18 +676,18 @@ class PFTabsContainer(wx.Panel):
 
     def OnLeftDown(self, event):
         mposx,mposy = event.GetPosition()
-        if self.showAddButton:
-            if not self.startDrag:
-                tab = self.FindTabAtPos(mposx, mposy)
-                if tab:
-                    self.CheckTabSelected(tab, mposx, mposy)
+        if not self.startDrag:
+            tab = self.FindTabAtPos(mposx, mposy)
+            if tab:
+                self.CheckTabSelected(tab, mposx, mposy)
+                if self.showAddButton:
                     self.startDrag = True
                     tx,ty = tab.GetPosition()
                     self.dragx = mposx - tx
                     self.dragy = self.containerHeight - self.height
-                    self.Refresh()
+                self.Refresh()
 
-                self.draggedTab = tab
+            self.draggedTab = tab
 
     def OnLeftUp(self, event):
         mposx,mposy = event.GetPosition()
