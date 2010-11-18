35d4c4e10a5b197cdc99ef0da9692e61aa03ccc7
HomeWorld <homeworld@gmail.com>
2010-11-18 15:18:28 +0200

    Border fix

--- gui/chromeTabs.py
+++ gui/chromeTabs.py
@@ -122,8 +122,8 @@ class PFNotebook(wx.Panel):
         bx, by = self.GetBorders()
 
         ww,wh = wsize
-        ww -= bx * 6 if 'wxMSW' in wx.PlatformInfo else 4
-        wh -= by * 6 if 'wxMSW' in wx.PlatformInfo else 4
+        ww -= bx * 4 if 'wxGTK' in wx.PlatformInfo else 6
+        wh -= by * 4 if 'wxGTK' in wx.PlatformInfo else 6
         page.SetSize((ww,wh))
         page.SetPosition((bx,by))
 
@@ -197,8 +197,8 @@ class PFNotebook(wx.Panel):
         size = self.pageContainer.GetSize()
         bx, by = self.GetBorders()
         ww,wh = size
-        ww -= bx * 6 if 'wxMSW' in wx.PlatformInfo else 4
-        wh -= by * 6 if 'wxMSW' in wx.PlatformInfo else 4
+        ww -= bx * 4 if 'wxGTK' in wx.PlatformInfo else 6
+        wh -= by * 4 if 'wxGTK' in wx.PlatformInfo else 6
         self.activePage.SetSize((ww,wh))
         self.activePage.SetPosition((bx,by))
         self.activePage.Show()
@@ -240,8 +240,8 @@ class PFNotebook(wx.Panel):
         size = self.pageContainer.GetSize()
         bx,by = self.GetBorders()
         ww,wh = size
-        ww -= bx * 6 if 'wxMSW' in wx.PlatformInfo else 4
-        wh -= by * 6 if 'wxMSW' in wx.PlatformInfo else 4
+        ww -= bx * 4 if 'wxGTK' in wx.PlatformInfo else 6
+        wh -= by * 4 if 'wxGTK' in wx.PlatformInfo else 6
 
         if self.activePage:
             self.activePage.SetSize((ww,wh))
