d17d5428b5765b77fc7b8837fb27cb18e3f960ca
Corollax <corollax@gmail.com>
2010-11-19 05:47:58 -0600

    Comments are for comments, not for old lines
    Remove unnecessary lines, don't comment them out.

--- gui/fittingView.py
+++ gui/fittingView.py
@@ -91,7 +91,6 @@ class FittingView(d.Display):
         self.Show(False)
         self.parent = parent
         self.mainFrame.Bind(FIT_CHANGED, self.fitChanged)
-#        self.mainFrame.Bind(gui.shipBrowser.EVT_FIT_SELECTED, self.fitSelected)
         self.mainFrame.Bind(gui.shipBrowser.EVT_FIT_RENAMED, self.fitRenamed)
         self.mainFrame.Bind(gui.shipBrowser.EVT_FIT_REMOVED, self.fitRemoved)
         self.mainFrame.Bind(gui.marketBrowser.ITEM_SELECTED, self.appendItem)
@@ -118,7 +117,6 @@ class FittingView(d.Display):
     def Destroy(self):
         self.parent.Unbind(gui.chromeTabs.EVT_NOTEBOOK_PAGE_CHANGED, handler=self.pageChanged)
         self.mainFrame.Unbind(FIT_CHANGED, handler=self.fitChanged)
-#        self.mainFrame.Unbind(gui.shipBrowser.EVT_FIT_SELECTED, handler=self.fitSelected)
         self.mainFrame.Unbind(gui.shipBrowser.EVT_FIT_RENAMED, handler=self.fitRenamed)
         self.mainFrame.Unbind(gui.shipBrowser.EVT_FIT_REMOVED, handler=self.fitRemoved)
         self.mainFrame.Unbind(gui.marketBrowser.ITEM_SELECTED, handler=self.appendItem)
