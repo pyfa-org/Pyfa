537a8ec7f014a5dcc2b657c3cfecfbf98fd4c9c0
HomeWorld <homeworld@gmail.com>
2010-12-06 15:36:58 +0200

    Updated aboutData to include ingame nicks, this closes ticket #323

--- aboutData.py
+++ aboutData.py
@@ -20,6 +20,6 @@
 versionString = "1.0 Stable RC1 - INCURSION"
 license = "pyfa is released under GNU GPL"
 licenseLocation = "gpl.txt"
-developers = ("cncfanatics" , "DarkPhoenix", "Darriele")
-credits = (("Entity: capacitor calculations"), ("Aurora: maths"), ("Corollax: Various improvements"))
+developers = ("\n cncfanatics \t(Sakari Orisi)\n" , " DarkPhoenix \t(Kadesh Priestess)\n", " Darriele \t(Darriele)")
+credits = (("Entity (Entity) \t\tcapacitor calculations"), ("Aurora \t\tmaths"), ("Corollax (Aamrr) \tVarious improvements"))
 description = "pyfa is the Python Fitting Assistant, a standalone application to create fittings for the EVE-Online SciFi MMORPG.\nThe application is available for many platforms, see the download page for further details. The layout of pyfa is heavily based on EFT, the EVE fitting tool. Although it is a complete rewrite and replacement for EFT, the fittings are still compatible with EFT.\n\nAll EVE-Online related materials are property of CCP hf.\n\nSilk Icons Set by famfamfam.com released under Creative Commons Attribution 2.5 License"
