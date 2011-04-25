3af87925568c8e7ec75a5e07bb5e7afbe659683d
DarkPhoenix <phoenix@mail.ru>
2011-04-25 10:41:55 +0400

    Revert "Implement basic context meenu sorting (by source context only for now)"
    This reverts commit a2bba4819061ef83dfd5af35297d77369c9ee7a7.

--- gui/builtinContextMenus/__init__.py
+++ gui/builtinContextMenus/__init__.py
@@ -1,3 +1,2 @@
 __all__ = ["moduleAmmoPicker", "itemStats", "damagePattern", "marketJump", "droneSplit",
            "ammoPattern", "project", "factorReload"]
-srcContextOrder = ("fittingModule", "fittingCharge", "fittingShip")
