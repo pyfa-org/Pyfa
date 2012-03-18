2a262e447ac5fd3eb51f8c03e3353ee915a74de7
HomeWorld <homeworld@gmail.com>
2012-03-18 20:03:52 +0200

    Use some black magic for 'tooltip' window show to avoid parent window (pyfa) losing focus (all blames goes to Entity)

--- gui/builtinViews/fittingView.py
+++ gui/builtinViews/fittingView.py
@@ -698,9 +698,10 @@ class PFToolTipWindow(wx.Frame):
         ty += self.padding * 2
 
         self.SetSize((tx,ty))
-
+        self.Disable()
         self.SetTransparent(0)
         self.Refresh()
+
 
     def OnTimer(self, event):
         self.transp += 20*self.direction
@@ -729,6 +730,7 @@ class PFToolTipWindow(wx.Frame):
     def Show(self, showWnd = True):
         if showWnd:
             wx.Frame.Show(self, showWnd)
+            self.Enable()
 #            self.RaiseParent()
             self.direction = 1
             self.timer.Start(10)
