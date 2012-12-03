e75e0e3fa4078adececf06df37e4d6dba80863eb
Sakari Orisi <sakari@evefit.org>
2012-12-03 22:43:05 +0100

    Add salvage drones group for sorting

--- gui/droneView.py
+++ gui/droneView.py
@@ -140,7 +140,7 @@ class DroneView(d.Display):
     DRONE_ORDER = ('Light Scout Drones', 'Medium Scout Drones',
                    'Heavy Attack Drones', 'Sentry Drones', 'Fighters',
                    'Fighter Bombers', 'Combat Utility Drones',
-                   'Electronic Warfare Drones', 'Logistic Drones', 'Mining Drones')
+                   'Electronic Warfare Drones', 'Logistic Drones', 'Mining Drones', 'Salvage Drones')
     def droneKey(self, drone):
         sMarket = service.Market.getInstance()
 
