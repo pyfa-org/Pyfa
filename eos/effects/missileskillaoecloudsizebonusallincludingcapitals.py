9fe93f46ef8e2acb3774c4b6a56c77794d7359ae
DarkPhoenix <phoenix@mail.ru>
2013-08-27 00:29:39 +0400

    Fix crash booster effect

--- eos/effects/missileskillaoecloudsizebonusallincludingcapitals.py
+++ eos/effects/missileskillaoecloudsizebonusallincludingcapitals.py
@@ -2,5 +2,5 @@
 # Implants named like: Crash Booster (4 of 4)
 type = "passive"
 def handler(fit, implant, context):
-    fit.modules.filteredChargeBoost(lambda mod: mod.charge.requiresSkill("Citadel Cruise Missiles") or mod.charge.requiresSkill("Citadel Torpedoes"),
+    fit.modules.filteredChargeBoost(lambda mod: mod.charge.requiresSkill("Missile Launcher Operation"),
                                     "aoeCloudSize", implant.getModifiedItemAttr("aoeCloudSizeBonus"))
