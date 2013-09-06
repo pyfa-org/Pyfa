5c1b39b492939f4a6ff85da55996d7df95170b59
DarkPhoenix <phoenix@mail.ru>
2013-09-06 21:26:13 +0400

    Fix kestrel effect

--- eos/effects/shipmissilevelocitycf.py
+++ eos/effects/shipmissilevelocitycf.py
@@ -2,6 +2,6 @@
 # Ship: Kestrel
 type = "passive"
 def handler(fit, ship, context):
-    level = fit.character.getSkill("Destroyers").level
+    level = fit.character.getSkill("Caldari Frigate").level
     fit.modules.filteredChargeBoost(lambda mod: mod.charge.requiresSkill("Missile Launcher Operation"),
                                     "maxVelocity", ship.getModifiedItemAttr("shipBonusCF") * level)
