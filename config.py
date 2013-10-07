8a663ef8edc29857dfcc06c4f63cdadd1ac163ac
DarkPhoenix <phoenix@mail.ru>
2013-10-08 01:18:14 +0400

    Update version in config

--- config.py
+++ config.py
@@ -15,8 +15,8 @@ saveInRoot = False
 # Version data
 version = "1.1.16"
 tag = "git"
-expansionName = "Odyssey"
-expansionVersion = "1.1"
+expansionName = "Singularity"
+expansionVersion = "621408"
 evemonMinVersion = "4081"
 
 pyfaPath = None
