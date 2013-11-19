666f4c9adb3944b4fea13ab15b7a8617861db848
DarkPhoenix <phoenix@mail.ru>
2013-11-19 22:04:02 +0400

    Make armor resistance bonus of heretic to rely on correct skill

--- eos/effects/elitebonusinterdictorsarmorresist1.py
+++ eos/effects/elitebonusinterdictorsarmorresist1.py
@@ -2,7 +2,7 @@
 # Ship: Heretic
 type = "passive"
 def handler(fit, ship, context):
-    level = fit.character.getSkill("Assault Frigates").level
+    level = fit.character.getSkill("Interdictors").level
     for damageType in ("Em", "Thermal", "Explosive", "Kinetic"):
         fit.ship.boostItemAttr("armor%sDamageResonance" % damageType,
                                ship.getModifiedItemAttr("eliteBonusInterdictors1") * level)
