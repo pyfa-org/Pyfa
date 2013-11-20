388c3c8bf19a7a3a6d39e1563471990358174ade
DarkPhoenix <phoenix@mail.ru>
2013-11-20 22:17:09 +0400

    Add missing Kronos falloff effect

---
+++ eos/effects/shipbonushtfalloffgb2.py
@@ -0,0 +1,7 @@
+# Used by:
+# Ship: Kronos
+type = "passive"
+def handler(fit, ship, context):
+    level = fit.character.getSkill("Gallente Battleship").level
+    fit.modules.filteredItemBoost(lambda mod: mod.item.requiresSkill("Large Hybrid Turret"),
+                                  "falloff", ship.getModifiedItemAttr("shipBonusGB2") * level)
