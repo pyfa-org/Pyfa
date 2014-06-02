dcd2cdeddc8bfe4717bf58bc153f710eae49829d
DarkPhoenix <phoenix@mail.ru>
2014-06-03 00:04:30 +0400

    Fix couple of ewar link effects, add missing SBA heat effect

--- eos/effects/commandbonusrsdmultiplywithcommandbonushidden.py
+++ eos/effects/commandbonusrsdmultiplywithcommandbonushidden.py
@@ -5,8 +5,8 @@ gangBoost = "ewarStrRSD"
 type = "active", "gang"
 def handler(fit, module, context):
     if "gang" not in context: return
-    fit.modules.filteredItemBoost(lambda mod: mod.item.group.name == "Remote Sensor Damper",
+    fit.modules.filteredItemBoost(lambda mod: mod.item.requiresSkill("Sensor Linking"),
                                   "maxTargetRangeBonus", module.getModifiedItemAttr("commandBonusRSD"))
-    fit.modules.filteredItemBoost(lambda mod: mod.item.group.name == "Remote Sensor Damper",
+    fit.modules.filteredItemBoost(lambda mod: mod.item.requiresSkill("Sensor Linking"),
                                   "scanResolutionBonus", module.getModifiedItemAttr("commandBonusRSD"),
                                   stackingPenalties=True)
