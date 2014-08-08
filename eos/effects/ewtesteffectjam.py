d29b4d91e9f6622a8c0d8c80e8291a2391787107
blitzmann <ryan.xgamer99@gmail.com>
2014-08-07 22:47:59 -0400

    Adds "Chance to Jam" to Sensor Strength tooltip

--- eos/effects/ewtesteffectjam.py
+++ eos/effects/ewtesteffectjam.py
@@ -2,5 +2,9 @@
 # Modules from group: ECM (44 of 44)
 # Drones named like: EC (3 of 3)
 type = "projected", "active"
-def handler(fit, container, context):
-    pass
+def handler(fit, module, context):
+    if "projected" in context:
+        # jam formula: 1 - (1- (jammer str/ship str))^(# of jam mods with same str))
+        strModifier = 1 - module.getModifiedItemAttr("scan{0}StrengthBonus".format(fit.scanType))/fit.scanStrength
+
+        fit.ecmProjectedStr *= strModifier
