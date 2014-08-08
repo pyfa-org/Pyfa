356c741e548dae685a37ceaddec8f2a6bf38b4c3
blitzmann <ryan.xgamer99@gmail.com>
2014-08-07 21:13:18 -0400

    Fixes #151

--- eos/effects/shieldboosterdurationbonusshieldskills.py
+++ eos/effects/shieldboosterdurationbonusshieldskills.py
@@ -2,5 +2,5 @@
 # Modules named like: Core Defense Operational Solidifier (8 of 8)
 type = "passive"
 def handler(fit, module, context):
-    fit.modules.filteredItemBoost(lambda mod: mod.item.requiresSkill("Shield Operation"),
+    fit.modules.filteredItemBoost(lambda mod: mod.item.requiresSkill("Shield Operation") or mod.item.requiresSkill("Capital Shield Operation"),
                                   "duration", module.getModifiedItemAttr("durationSkillBonus"))
