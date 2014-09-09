c262acd0dce0c80cae41b04aa6a00967a49d6321
DarkPhoenix <phoenix@mail.ru>
2014-09-09 15:44:21 +0400

    RR drone rigs are penalized

--- eos/effects/repairdronearmordamageamountbonus.py
+++ eos/effects/repairdronearmordamageamountbonus.py
@@ -5,4 +5,5 @@ type = "passive"
 def handler(fit, container, context):
     level = container.level if "skill" in context else 1
     fit.drones.filteredItemBoost(lambda drone: drone.item.group.name == "Logistic Drone",
-                                 "armorDamageAmount", container.getModifiedItemAttr("damageHP") * level)
+                                 "armorDamageAmount", container.getModifiedItemAttr("damageHP") * level,
+                                 stackingPenalties = True)
