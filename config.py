9b8c1ff3ba5b37e9b8c89e9baae53468a2dcd34a
blitzmann <ryan.xgamer99@gmail.com>
2014-09-30 21:56:54 -0400

    Bump dev cycle

--- config.py
+++ config.py
@@ -13,8 +13,8 @@ debug = False
 saveInRoot = False
 
 # Version data
-version = "1.5.0"
-tag = "Stable"
+version = "1.5.1"
+tag = "git"
 expansionName = "Oceanus"
 expansionVersion = "1.0"
 evemonMinVersion = "4081"
