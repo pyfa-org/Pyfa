cb27efd5e1f78250203e2ad6fa6f324828fec383
DarkPhoenix <phoenix@mail.ru>
2014-11-07 01:33:57 +0300

    Mark next development cycle

--- config.py
+++ config.py
@@ -13,8 +13,8 @@ debug = False
 saveInRoot = False
 
 # Version data
-version = "1.6.0"
-tag = "Stable"
+version = "1.6.1"
+tag = "git"
 expansionName = "Phoebe"
 expansionVersion = "1.0"
 evemonMinVersion = "4081"
