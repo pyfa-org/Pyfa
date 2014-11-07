7641384abafb59a5713e8afe2097a3179e12664e
DarkPhoenix <phoenix@mail.ru>
2014-11-07 12:52:01 +0300

    Remove unused import

--- eos/db/gamedata/attribute.py
+++ eos/db/gamedata/attribute.py
@@ -18,7 +18,7 @@
 #===============================================================================
 
 from sqlalchemy import Table, Column, Integer, Float, Unicode, ForeignKey, String, Boolean
-from sqlalchemy.orm import relation, mapper, synonym, deferred, remote
+from sqlalchemy.orm import relation, mapper, synonym, deferred
 from sqlalchemy.ext.associationproxy import association_proxy
 from eos.types import Attribute, Icon, AttributeInfo, Unit
 from eos.db import gamedata_meta
