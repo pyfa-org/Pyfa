36a3ac70b6094a008721ee2a0104c132c501004b
blitzmann <ryan.xgamer99@gmail.com>
2015-01-26 23:51:46 -0500

    Removed unneeded variable in effect file

--- eos/effects/modesigradiuspostdiv.py
+++ eos/effects/modesigradiuspostdiv.py
@@ -4,6 +4,5 @@
 # Module: Amarr Tactical Destroyer Defense Mode
 type = "passive"
 def handler(fit, module, context):
-    level = fit.character.getSkill("Minmatar Destroyer").level
     fit.ship.multiplyItemAttr("signatureRadius", 1/module.getModifiedItemAttr("modeSignatureRadiusPostDiv"),
                            stackingPenalties = True, penaltyGroup="postDiv")
