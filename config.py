ef62f102bc6f86f6fe7f798aa5a7769b20c7271d
blitzmann <ryan.xgamer99@gmail.com>
2015-02-07 21:15:23 -0500

    Fix #256

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ evemonMinVersion = "4081"
 
 # Database version (int ONLY)
 # Increment every time we need to flag for user database upgrade/modification
-dbversion = 4
+dbversion = 5
 
 pyfaPath = None
 savePath = None
---
+++ eos/db/migrations/upgrade5.py
@@ -0,0 +1,8 @@
+"""
+Migration 5
+
+Simply deletes damage profiles with a blank name. See GH issue #256
+"""
+
+def upgrade(saveddata_engine):
+    saveddata_engine.execute('DELETE FROM damagePatterns WHERE name LIKE ?', ("",))
