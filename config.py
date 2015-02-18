b16d2eae15c21922606faefb46ef75ce16bd4a97
DarkPhoenix <phoenix@mail.ru>
2015-02-18 15:48:40 +0300

    Bump for a next development release

--- config.py
+++ config.py
@@ -13,8 +13,8 @@ debug = False
 saveInRoot = False
 
 # Version data
-version = "1.9.0"
-tag = "Stable"
+version = "1.9.1"
+tag = "git"
 expansionName = "Tiamat"
 expansionVersion = "1.0"
 evemonMinVersion = "4081"
