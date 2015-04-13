940035fa8cd484d0b79cb88077596ae9f311a6a5
DarkPhoenix <phoenix@mail.ru>
2015-04-13 15:48:35 +0300

    Fix market groups of civilian modules

--- service/market.py
+++ service/market.py
@@ -274,18 +274,18 @@ class Market():
             "'Daemon' Data Analyzer I": 714, # Ship Equipment > Electronics and Sensor Upgrades > Scanners > Data and Composition Scanners
             "'Libram' Data Analyzer I": 714, # Ship Equipment > Electronics and Sensor Upgrades > Scanners > Data and Composition Scanners
             "Advanced Cerebral Accelerator": 977, # Implants & Boosters > Booster
-            "Civilian Damage Control": 760, # Ship Equipment > Civilian Modules
-            "Civilian EM Ward Field": 760, # Ship Equipment > Civilian Modules
-            "Civilian Explosive Deflection Field": 760, # Ship Equipment > Civilian Modules
+            "Civilian Damage Control": 615, # Ship Equipment > Hull & Armor > Damage Controls
+            "Civilian EM Ward Field": 1695, # Ship Equipment > Shield > Shield Hardeners > EM Shield Hardeners
+            "Civilian Explosive Deflection Field": 1694, # Ship Equipment > Shield > Shield Hardeners > Explosive Shield Hardeners
             "Civilian Hobgoblin": 837, # Drones > Combat Drones > Light Scout Drones
-            "Civilian Kinetic Deflection Field": 760, # Ship Equipment > Civilian Modules
-            "Civilian Light Missile Launcher": 760, # Ship Equipment > Civilian Modules
+            "Civilian Kinetic Deflection Field": 1693, # Ship Equipment > Shield > Shield Hardeners > Kinetic Shield Hardeners
+            "Civilian Light Missile Launcher": 640, # Ship Equipment > Turrets & Bays > Missile Launchers > Light Missile Launchers
             "Civilian Scourge Light Missile": 920, # Ammunition & Charges > Missiles > Light Missiles > Standard Light Missiles
-            "Civilian Small Remote Armor Repairer": 760, # Ship Equipment > Civilian Modules
-            "Civilian Small Remote Shield Booster": 760, # Ship Equipment > Civilian Modules
-            "Civilian Stasis Webifier": 760, # Ship Equipment > Civilian Modules
-            "Civilian Thermic Dissipation Field": 760, # Ship Equipment > Civilian Modules
-            "Civilian Warp Disruptor": 760, # Ship Equipment > Civilian Modules
+            "Civilian Small Remote Armor Repairer": 1059, # Ship Equipment > Hull & Armor > Remote Armor Repairers > Small
+            "Civilian Small Remote Shield Booster": 603, # Ship Equipment > Shield > Remote Shield Boosters > Small
+            "Civilian Stasis Webifier": 683, # Ship Equipment > Electronic Warfare > Stasis Webifiers
+            "Civilian Thermic Dissipation Field": 1692, # Ship Equipment > Shield > Shield Hardeners > Thermal Shield Hardeners
+            "Civilian Warp Disruptor": 1935, # Ship Equipment > Electronic Warfare > Warp Disruptors
             "Hardwiring - Zainou 'Sharpshooter' ZMX10": 1493, # Implants & Boosters > Implants > Skill Hardwiring > Missile Implants > Implant Slot 06
             "Hardwiring - Zainou 'Sharpshooter' ZMX100": 1493, # Implants & Boosters > Implants > Skill Hardwiring > Missile Implants > Implant Slot 06
             "Hardwiring - Zainou 'Sharpshooter' ZMX1000": 1493, # Implants & Boosters > Implants > Skill Hardwiring > Missile Implants > Implant Slot 06
