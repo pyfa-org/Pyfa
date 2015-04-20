e5430cac8463345c71b558a0564acb5fedce7842
DarkPhoenix <phoenix@mail.ru>
2015-04-20 15:04:47 +0300

    Do not apply ES bonus to weapon locus rigs

--- eos/effects/commandbonustdmultiplywithcommandbonushidden.py
+++ eos/effects/commandbonustdmultiplywithcommandbonushidden.py
@@ -8,5 +8,5 @@ type = "active", "gang"
 def handler(fit, module, context):
     if "gang" not in context: return
     for bonus in ("maxRangeBonus", "falloffBonus", "trackingSpeedBonus"):
-        fit.modules.filteredItemBoost(lambda mod: lambda mod: mod.item.requiresSkill("Weapon Disruption"),
+        fit.modules.filteredItemBoost(lambda mod: mod.item.requiresSkill("Weapon Disruption"),
                                       bonus, module.getModifiedItemAttr("commandBonusTD"))
