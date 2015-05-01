4e69a2656c83e211e4536c65c8c4f2ef11f500b4
DarkPhoenix <phoenix@mail.ru>
2015-05-01 15:37:09 +0300

    Add some options for Mac binary generation
    Can't test it atm, but whatever

--- setup.py
+++ setup.py
@@ -21,15 +21,11 @@ app_description = 'Python fitting assistant'
 packages = ['eos', 'gui', 'service', 'utils']
 include_files = ['icons', 'staticdata', 'gpl.txt']
 includes = []
-excludes = ['Tkinter', 'setup']
+excludes = ['Tkinter']
 
 
-def dict_union(a, b):
-    """Values from b have priority"""
-    return dict(i for i in chain(a.items(), b.items()))
-    
-
-build_options_generic = {
+# Windows-specific options
+build_options_winexe = {
     'packages': packages,
     'include_files': include_files,
     'includes': includes,
@@ -38,22 +34,36 @@ build_options_generic = {
     'optimize': 2
 }
 
-build_options_winexe = dict_union(build_options_generic, {
-})
-
 build_options_winmsi = {
     'upgrade_code': '{E80885AC-31BA-4D9A-A04F-9E5915608A6C}',
     'add_to_path': False,
     'initial_target_dir': r'[ProgramFilesFolder]\{}'.format(app_name),
 }
 
+
+# Mac-specific options (untested)
+build_options_macapp = {
+    'iconfile': 'dist_assets/mac/pyfa.icns',
+    'bundle_name': app_name,
+}
+
+build_options_macdmg = {
+    'volume_label': app_name,
+    'applications-shortcut': True,
+}
+
+
+# Generic executable options
 executable_options = {
     'script': 'pyfa.py',
+    # Following are windows-specific options, they are stored
+    # on a per-executable basis
     'base': 'Win32GUI' if sys.platform=='win32' else None,
     'icon': 'dist_assets/win/pyfa.ico',
     'shortcutDir': 'DesktopFolder',
     'shortcutName': app_name,
 }
+
 
 setup(
     name=app_name,
@@ -61,7 +71,9 @@ setup(
     description=app_description,
     options = {
         'build_exe': build_options_winexe,
-        'bdist_msi': build_options_winmsi
+        'bdist_msi': build_options_winmsi,
+        'bdist_mac': build_options_macapp,
+        'bdist_dmg': build_options_macdmg,
     },
     executables=[Executable(**executable_options)]
 )
