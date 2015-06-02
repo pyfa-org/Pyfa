1d4510210086abeb72624791ad4389c1b3bcc343
DarkPhoenix <phoenix@mail.ru>
2015-06-02 23:32:16 +0300

    Bump for a stable release

--- config.py
+++ config.py
@@ -13,8 +13,8 @@ debug = False
 saveInRoot = False
 
 # Version data
-version = "1.11.1"
-tag = "git"
+version = "1.12.0"
+tag = "Stable"
 expansionName = "Carnyx"
 expansionVersion = "1.0"
 evemonMinVersion = "4081"
