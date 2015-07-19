90c883da67a872dd7b44c5d8dbf0e96f295dd8f9
blitzmann <holmes.ryan.90@gmail.com>
2015-07-19 15:51:03 -0400

    Fix some syntax errors.

--- eos/effects/caldarishipewcapacitorneedcc.py
+++ eos/effects/caldarishipewcapacitorneedcc.py
@@ -7,4 +7,4 @@
 type = "passive"
 def handler(fit, ship, context):
     fit.modules.filteredItemBoost(lambda mod: mod.item.group.name == "ECM",
-                                  "capacitorNeed", ship.getModifiedItemAttr("shipBonusCC", skill="Caldari Cruiser")
+                                  "capacitorNeed", ship.getModifiedItemAttr("shipBonusCC"), skill="Caldari Cruiser")
--- eos/effects/shipbonusscanprobestrengthgf.py
+++ eos/effects/shipbonusscanprobestrengthgf.py
@@ -5,4 +5,4 @@
 type = "passive"
 def handler(fit, ship, context):
     fit.modules.filteredChargeBoost(lambda mod: mod.charge.group.name == "Scanner Probe",
-                                    "baseSensorStrength", ship.getModifiedItemAttr("shipBonusGF2", skill="Gallente Frigate"l)
+                                    "baseSensorStrength", ship.getModifiedItemAttr("shipBonusGF2"), skill="Gallente Frigate")
