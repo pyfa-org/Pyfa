0a1f32405376ae62dce878d371c2ffa73eeb8514
blitzmann <holmes.ryan.90@gmail.com>
2015-07-21 12:27:46 -0400

    Fix Advanced Spaceship Command skill and resists for Occator

--- eos/effects/eliteindustrialarmorresists2.py
+++ eos/effects/eliteindustrialarmorresists2.py
@@ -5,5 +5,6 @@
 # Ship: Occator
 type = "passive"
 def handler(fit, ship, context):
+    for damageType in ("em", "thermal", "explosive", "kinetic"):
         fit.ship.boostItemAttr("armor{}DamageResonance".format(damageType.capitalize()),
                                ship.getModifiedItemAttr("eliteBonusIndustrial2"), skill="Transport Ships")
--- eos/effects/shipadvancedspaceshipcommandagilitybonus.py
+++ eos/effects/shipadvancedspaceshipcommandagilitybonus.py
@@ -4,4 +4,6 @@
 # Items from market group: Ships > Capital Ships (27 of 28)
 type = "passive"
 def handler(fit, ship, context):
-    fit.ship.boostItemAttr("agility", skill.getModifiedItemAttr("agilityBonus"), skill="Advanced Spaceship Command")
+    skillName = "Advanced Spaceship Command"
+    skill = fit.character.getSkill(skillName)
+    fit.ship.boostItemAttr("agility", skill.getModifiedItemAttr("agilityBonus"), skill=skillName)
