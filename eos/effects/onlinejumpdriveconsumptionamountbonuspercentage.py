8647fa245f578c46e96c16af821402c6aecf516c
blitzmann <ryan.xgamer99@gmail.com>
2015-09-24 13:09:43 -0400

    Fix Jump Drive Economizer. It does not project, and is only applies bonus when online.

--- eos/effects/onlinejumpdriveconsumptionamountbonuspercentage.py
+++ eos/effects/onlinejumpdriveconsumptionamountbonuspercentage.py
@@ -3,6 +3,6 @@
 # Used by:
 # Modules from group: Jump Drive Economizer (3 of 3)
 runTime = "early"
-type = ("projected", "offline")
+type = ("projected", "passive")
 def handler(fit, module, context):
     fit.ship.boostItemAttr("jumpDriveConsumptionAmount", module.getModifiedItemAttr("consumptionQuantityBonusPercentage"), stackingPenalties=True)
