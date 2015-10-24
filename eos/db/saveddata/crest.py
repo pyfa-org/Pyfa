a5920a9c9ce200cf99c5d5f01e47a07c823b7cc6
blitzmann <ryan.xgamer99@gmail.com>
2015-10-24 16:46:45 -0400

    Added missing files (and fix why they were missing)

---
+++ eos/db/saveddata/crest.py
@@ -0,0 +1,31 @@
+#===============================================================================
+# Copyright (C) 2010 Diego Duclos
+#
+# This file is part of eos.
+#
+# eos is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# eos is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with eos.  If not, see <http://www.gnu.org/licenses/>.
+#===============================================================================
+
+from sqlalchemy import Table, Column, Integer, String, Boolean
+from sqlalchemy.orm import mapper
+
+from eos.db import saveddata_meta
+from eos.types import Crest
+
+crest_table = Table("crest", saveddata_meta,
+                         Column("ID", Integer, primary_key = True),
+                         Column("name", String, nullable = False, unique = True),
+                         Column("refresh_token", String, nullable = False))
+
+mapper(Crest, crest_table)
