331338e7b9aa068a08082ea0e73924e6bb774200
blitzmann <ryan.xgamer99@gmail.com>
2015-12-30 23:02:27 -0500

    Fix Command Destroyer MWD sig radius issue

--- eos/effects/elitebonuscommanddestroyermwdsigradius3.py
+++ eos/effects/elitebonuscommanddestroyermwdsigradius3.py
@@ -4,4 +4,4 @@
 # Ships from group: Command Destroyers (4 of 4)
 type = "passive"
 def handler(fit, src, context):
-    fit.modules.filteredItemBoost(lambda mod: mod.item.requiresSkill("High Speed Maneuvering"), "signatureRadiusBonus", src.getModifiedItemAttr("eliteBonusCommandDestroyer3"), skill="Command Destroyers")
+    fit.modules.filteredItemBoost(lambda mod: mod.item.requiresSkill("High Speed Maneuvering"), "signatureRadiusBonus", src.getModifiedItemAttr("eliteBonusCommandDestroyer3") * 5)  # * 5 because eliteBonusCommandDestroyer3 is only -10 when it should be -50
