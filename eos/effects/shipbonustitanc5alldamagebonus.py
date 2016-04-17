b66e98f697c3642151ed3e53f177eab1860e962d
blitzmann <ryan.xgamer99@gmail.com>
2016-04-17 00:10:16 -0400

    Fix Leviathan effect

--- eos/effects/shipbonustitanc5alldamagebonus.py
+++ eos/effects/shipbonustitanc5alldamagebonus.py
@@ -4,12 +4,12 @@
 # Ship: Leviathan
 type = "passive"
 def handler(fit, src, context):
-    fit.modules.filteredChargeBoost(lambda mod: mod.charge.requiresSkill("XL Torpedoes"), "thermalDamage", src.getModifiedItemAttr("shipBonusDreadnoughtC1"), skill="Caldari Dreadnought")
-    fit.modules.filteredChargeBoost(lambda mod: mod.charge.requiresSkill("Torpedoes"), "emDamage", src.getModifiedItemAttr("shipBonusDreadnoughtC1"), skill="Caldari Dreadnought")
-    fit.modules.filteredChargeBoost(lambda mod: mod.charge.requiresSkill("Torpedoes"), "explosiveDamage", src.getModifiedItemAttr("shipBonusDreadnoughtC1"), skill="Caldari Dreadnought")
-    fit.modules.filteredChargeBoost(lambda mod: mod.charge.requiresSkill("XL Cruise Missiles"), "explosiveDamage", src.getModifiedItemAttr("shipBonusDreadnoughtC1"), skill="Caldari Dreadnought")
-    fit.modules.filteredChargeBoost(lambda mod: mod.charge.requiresSkill("XL Cruise Missiles"), "thermalDamage", src.getModifiedItemAttr("shipBonusDreadnoughtC1"), skill="Caldari Dreadnought")
-    fit.modules.filteredChargeBoost(lambda mod: mod.charge.requiresSkill("Torpedoes"), "thermalDamage", src.getModifiedItemAttr("shipBonusDreadnoughtC1"), skill="Caldari Dreadnought")
-    fit.modules.filteredChargeBoost(lambda mod: mod.charge.requiresSkill("XL Torpedoes"), "emDamage", src.getModifiedItemAttr("shipBonusDreadnoughtC1"), skill="Caldari Dreadnought")
-    fit.modules.filteredChargeBoost(lambda mod: mod.charge.requiresSkill("XL Torpedoes"), "explosiveDamage", src.getModifiedItemAttr("shipBonusDreadnoughtC1"), skill="Caldari Dreadnought")
-    fit.modules.filteredChargeBoost(lambda mod: mod.charge.requiresSkill("XL Cruise Missiles"), "emDamage", src.getModifiedItemAttr("shipBonusDreadnoughtC1"), skill="Caldari Dreadnought")
+    fit.modules.filteredChargeBoost(lambda mod: mod.charge.requiresSkill("Torpedoes"), "emDamage", src.getModifiedItemAttr("shipBonusTitanC5"), skill="Caldari Titan")
+    fit.modules.filteredChargeBoost(lambda mod: mod.charge.requiresSkill("Torpedoes"), "explosiveDamage", src.getModifiedItemAttr("shipBonusTitanC5"), skill="Caldari Titan")
+    fit.modules.filteredChargeBoost(lambda mod: mod.charge.requiresSkill("Torpedoes"), "thermalDamage", src.getModifiedItemAttr("shipBonusTitanC5"), skill="Caldari Titan")
+    fit.modules.filteredChargeBoost(lambda mod: mod.charge.requiresSkill("XL Cruise Missiles"), "explosiveDamage", src.getModifiedItemAttr("shipBonusTitanC5"), skill="Caldari Titan")
+    fit.modules.filteredChargeBoost(lambda mod: mod.charge.requiresSkill("XL Cruise Missiles"), "thermalDamage", src.getModifiedItemAttr("shipBonusTitanC5"), skill="Caldari Titan")
+    fit.modules.filteredChargeBoost(lambda mod: mod.charge.requiresSkill("XL Cruise Missiles"), "emDamage", src.getModifiedItemAttr("shipBonusTitanC5"), skill="Caldari Titan")
+    fit.modules.filteredChargeBoost(lambda mod: mod.charge.requiresSkill("XL Torpedoes"), "thermalDamage", src.getModifiedItemAttr("shipBonusTitanC5"), skill="Caldari Titan")
+    fit.modules.filteredChargeBoost(lambda mod: mod.charge.requiresSkill("XL Torpedoes"), "emDamage", src.getModifiedItemAttr("shipBonusTitanC5"), skill="Caldari Titan")
+    fit.modules.filteredChargeBoost(lambda mod: mod.charge.requiresSkill("XL Torpedoes"), "explosiveDamage", src.getModifiedItemAttr("shipBonusTitanC5"), skill="Caldari Titan")
