26c27153f0fe1c127ca86a6306ea71b0601fe81d
blitzmann <ryan.xgamer99@gmail.com>
2016-04-29 19:11:13 -0400

    Fix FAUX role bonus

--- eos/effects/shipbonusforceauxiliaryrole2logisticdronebonus.py
+++ eos/effects/shipbonusforceauxiliaryrole2logisticdronebonus.py
@@ -4,6 +4,6 @@
 # Ships from group: Force Auxiliary (4 of 4)
 type = "passive"
 def handler(fit, src, context):
-    fit.modules.filteredChargeBoost(lambda mod: mod.charge.requiresSkill("Repair Drone Operation"), "structureDamageAmount", src.getModifiedItemAttr("shipBonusRole2"))
-    fit.modules.filteredChargeBoost(lambda mod: mod.charge.requiresSkill("Repair Drone Operation"), "armorDamageAmount", src.getModifiedItemAttr("shipBonusRole2"))
-    fit.modules.filteredChargeBoost(lambda mod: mod.charge.requiresSkill("Repair Drone Operation"), "shieldBonus", src.getModifiedItemAttr("shipBonusRole2"))
+    fit.drones.filteredItemBoost(lambda mod: mod.item.requiresSkill("Repair Drone Operation"), "structureDamageAmount", src.getModifiedItemAttr("shipBonusRole2"))
+    fit.drones.filteredItemBoost(lambda mod: mod.item.requiresSkill("Repair Drone Operation"), "armorDamageAmount", src.getModifiedItemAttr("shipBonusRole2"))
+    fit.drones.filteredItemBoost(lambda mod: mod.item.requiresSkill("Repair Drone Operation"), "shieldBonus", src.getModifiedItemAttr("shipBonusRole2"))
