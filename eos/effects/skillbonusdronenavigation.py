5a07be40f14c4a27b3de87ffff14d122e7f2eb70
blitzmann <ryan.xgamer99@gmail.com>
2016-05-07 20:02:24 -0400

    Update to 1037828 (Citadel 1.9)

--- eos/effects/skillbonusdronenavigation.py
+++ eos/effects/skillbonusdronenavigation.py
@@ -5,5 +5,5 @@
 type = "passive"
 def handler(fit, src, context):
     lvl = src.level
-    fit.drones.filteredItemBoost(lambda mod: mod.item.requiresSkill("Drones"), "maxVelocity", src.getModifiedItemAttr("droneMaxVelocityBonus") * lvl)
-    fit.fighters.filteredItemBoost(lambda mod: mod.item.requiresSkill("Fighters"), "maxVelocity", src.getModifiedItemAttr("droneMaxVelocityBonus") * lvl)
+    fit.drones.filteredItemBoost(lambda mod: mod.item.requiresSkill("Drones"), "maxVelocity", src.getModifiedItemAttr("maxVelocityBonus") * lvl)
+    fit.fighters.filteredItemBoost(lambda mod: mod.item.requiresSkill("Fighters"), "maxVelocity", src.getModifiedItemAttr("maxVelocityBonus") * lvl)
