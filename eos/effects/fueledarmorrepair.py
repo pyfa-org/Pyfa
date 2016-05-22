7406210c4a31e05a009df9ac7b73d7744b1cfb2a
blitzmann <ryan.xgamer99@gmail.com>
2016-05-22 01:38:40 -0400

    Fix remote AAR

--- eos/effects/fueledarmorrepair.py
+++ eos/effects/fueledarmorrepair.py
@@ -6,8 +6,10 @@ runTime = "late"
 type = "active"
 def handler(fit, module, context):
     if module.charge and module.charge.name == "Nanite Repair Paste":
-        module.multiplyItemAttr("armorDamageAmount", 3)
+        multiplier = 3
+    else:
+        multiplier = 1
         
-    amount = module.getModifiedItemAttr("armorDamageAmount")
+    amount = module.getModifiedItemAttr("armorDamageAmount") * multiplier
     speed = module.getModifiedItemAttr("duration") / 1000.0
     fit.extraAttributes.increase("armorRepair", amount / speed)
--- eos/effects/modulebonusancillaryremotearmorrepairer.py
+++ eos/effects/modulebonusancillaryremotearmorrepairer.py
@@ -2,10 +2,12 @@ runTime = "late"
 type = "projected", "active"
 def handler(fit, module, context):
     if "projected" not in context: return
-    
+
     if module.charge and module.charge.name == "Nanite Repair Paste":
-        module.multiplyItemAttr("armorDamageAmount", 3)
-        
-    amount = module.getModifiedItemAttr("armorDamageAmount")
+        multiplier = 3
+    else:
+        multiplier = 1
+
+    amount = module.getModifiedItemAttr("armorDamageAmount") * multiplier
     speed = module.getModifiedItemAttr("duration") / 1000.0
     fit.extraAttributes.increase("armorRepair", amount / speed)
