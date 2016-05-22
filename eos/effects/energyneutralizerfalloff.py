d04ea5b0615ee34a78ad646dbe7c534633d3c21a
blitzmann <ryan.xgamer99@gmail.com>
2016-05-22 13:32:59 -0400

    Fix EV drones (#619)

--- eos/effects/energyneutralizerfalloff.py
+++ eos/effects/energyneutralizerfalloff.py
@@ -8,7 +8,6 @@ type = "active", "projected"
 def handler(fit, container, context):
     if "projected" in context and ((hasattr(container, "state") \
     and container.state >= State.ACTIVE) or hasattr(container, "amountActive")):
-        multiplier = container.amountActive if hasattr(container, "amountActive") else 1
         amount = container.getModifiedItemAttr("energyDestabilizationAmount")
         time = container.getModifiedItemAttr("duration")
-        fit.addDrain(time, amount * multiplier, 0)
+        fit.addDrain(time, amount, 0)
