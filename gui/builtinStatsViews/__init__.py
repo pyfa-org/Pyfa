9b6c7c8817234753a1e8b8abb39353b2793ec2b8
Ebag333 <ebagola@gmail.com>
2016-06-17 00:03:42 -0700

    Initial build out

--- gui/builtinStatsViews/__init__.py
+++ gui/builtinStatsViews/__init__.py
@@ -1,3 +1,3 @@
 __all__ = ["resourcesViewFull", "resistancesViewFull",
            "rechargeViewFull", "firepowerViewFull", "capacitorViewFull",
-           "targetingMiscViewFull", "priceViewFull", "miningyieldViewFull"]
+           "targetingMiscViewFull", "priceViewFull", "miningyieldViewFull", "miscViewFull", "shipStatsViewFull"]
