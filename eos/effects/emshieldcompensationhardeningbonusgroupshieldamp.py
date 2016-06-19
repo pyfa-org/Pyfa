7a5e4291a521aecee0b1316b6bed39d6db6c004c
blitzmann <ryan.xgamer99@gmail.com>
2016-06-19 12:40:49 -0400

    Handle renamed groups

--- eos/effects/emshieldcompensationhardeningbonusgroupshieldamp.py
+++ eos/effects/emshieldcompensationhardeningbonusgroupshieldamp.py
@@ -4,5 +4,5 @@
 # Skill: EM Shield Compensation
 type = "passive"
 def handler(fit, skill, context):
-    fit.modules.filteredItemBoost(lambda mod: mod.item.group.name == "Shield Amplifier",
+    fit.modules.filteredItemBoost(lambda mod: mod.item.group.name == "Shield Resistance Amplifier",
                                   "emDamageResistanceBonus", skill.getModifiedItemAttr("hardeningBonus") * skill.level)
