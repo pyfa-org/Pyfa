ed3b2eb1bf96f1de82201b91a76257a3e39237a2
blitzmann <ryan.xgamer99@gmail.com>
2016-06-19 23:04:52 -0400

    Fix energy warfare

--- eos/effects/entityenergyneutralizerfalloff.py
+++ eos/effects/entityenergyneutralizerfalloff.py
@@ -1,12 +1,12 @@
-# energyNeutralizerEntity
+# energyNeutralizerFalloff
 #
 # Used by:
-# Drones from group: Energy Neutralizer Drone (3 of 3)
+# Modules from group: Energy Neutralizer (51 of 51)
 from eos.types import State
 type = "active", "projected"
 def handler(fit, container, context):
     if "projected" in context and ((hasattr(container, "state") \
     and container.state >= State.ACTIVE) or hasattr(container, "amountActive")):
         amount = container.getModifiedItemAttr("energyNeutralizerAmount")
-        time = container.getModifiedItemAttr("duration")
+        time = container.getModifiedItemAttr("energyNeutralizerDuration")
         fit.addDrain(time, amount, 0)
--- eos/effects/shipenergyneutralizertransferamountbonusaf2.py
+++ eos/effects/shipenergyneutralizertransferamountbonusaf2.py
@@ -5,4 +5,4 @@
 type = "passive"
 def handler(fit, ship, context):
     fit.modules.filteredItemBoost(lambda mod: mod.item.group.name == "Energy Neutralizer",
-                                  "energyDestabilizationAmount", ship.getModifiedItemAttr("shipBonus2AF"), skill="Amarr Frigate")
+                                  "energyNeutralizerAmount", ship.getModifiedItemAttr("shipBonus2AF"), skill="Amarr Frigate")
