4a70028762b84b76402323f932338d74f191d903
Ebag333 <ebagola@gmail.com>
2016-07-02 01:02:03 -0700

    Fixed type of weapon
    Used to be applied to Naglfar, but everyone knows that Vehement is the
    new Naglfar, and never use projectiles after memorial day (only
    hybrids).

--- eos/effects/shipbonusdreadnoughtrole1damagebonus.py
+++ eos/effects/shipbonusdreadnoughtrole1damagebonus.py
@@ -1,7 +1,9 @@
 # shipBonusDreadnoughtRole1DamageBonus
 #
 # Used by:
-# Ship: Naglfar
+# Ship: Vehement (used to be used by Naglfar)
 type = "passive"
-def handler(fit, src, context):
-    fit.modules.filteredItemBoost(lambda mod: mod.item.requiresSkill("Capital Projectile Turret"), "damageMultiplier", src.getModifiedItemAttr("shipBonusRole1"))
+def handler(fit, ship, context):
+    fit.modules.filteredItemBoost(lambda mod: mod.item.requiresSkill("Capital Hybrid Turret"),
+                                  "damageMultiplier", ship.getModifiedItemAttr("shipBonusRole1"))
+
