2212311a4491e2818c8aa9f7214218262d84bbe7
blitzmann <ryan.xgamer99@gmail.com>
2016-07-03 14:25:05 -0400

    Add effects for Asklepian implant set

---
+++ eos/effects/setbonusasklepian.py
@@ -0,0 +1,4 @@
+type = "passive"
+def handler(fit, src, context):
+    fit.appliedImplants.filteredItemMultiply(lambda mod: mod.item.requiresSkill("Cybernetics"),
+                                             "armorRepairBonus", src.getModifiedItemAttr("implantSetSerpentis2"))
