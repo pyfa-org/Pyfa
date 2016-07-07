8f64c91677cf8133f04210ebf598cacacc73319f
blitzmann <ryan.xgamer99@gmail.com>
2016-07-06 23:52:42 -0400

    Update effect headers

--- eos/effects/armorrepairamountbonussubcap.py
+++ eos/effects/armorrepairamountbonussubcap.py
@@ -1,3 +1,7 @@
+# armorRepairAmountBonusSubcap
+#
+# Used by:
+# Implants named like: Grade Asklepian (15 of 16)
 type = "passive"
 def handler(fit, src, context):
     fit.modules.filteredItemBoost(lambda mod: mod.item.requiresSkill("Repair Systems"),
