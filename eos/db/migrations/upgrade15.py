ed24ef261581ca6f841c3918dfaed4eeeb97262c
blitzman <ryan.xgamer99@gmail.com>
2016-08-28 20:22:30 -0400

    Delete projected modules from citadels

---
+++ eos/db/migrations/upgrade15.py
@@ -0,0 +1,21 @@
+"""
+Migration 15
+
+- Delete projected modules on citadels
+"""
+
+import sqlalchemy
+
+def upgrade(saveddata_engine):
+
+    sql = """
+    DELETE FROM modules WHERE ID IN
+    (
+        SELECT m.ID FROM modules AS m
+        JOIN fits AS f ON m.fitID = f.ID
+        WHERE f.shipID IN ("35832", "35833", "35834", "40340")
+        AND m.projected = 1
+    )
+    """
+
+    saveddata_engine.execute(sql)
