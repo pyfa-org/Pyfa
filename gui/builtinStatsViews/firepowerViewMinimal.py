267e343ea809c4fb73695d41e2120cc27d2f7b73
Ebag333 <ebagola@gmail.com>
2016-09-18 22:06:49 -0700

    Revert "Trying something"
    This reverts commit 9e8d114cc48e8e0db658d36263b46dfafe342677.

--- gui/builtinStatsViews/firepowerViewMinimal.py
+++ gui/builtinStatsViews/firepowerViewMinimal.py
@@ -25,7 +25,6 @@ from gui.bitmapLoader import BitmapLoader
 from gui.utils.numberFormatter import formatAmount
 from gui.contextMenu import ContextMenu
 
-
 class FirepowerViewMinimal(StatsView):
     name = "firepowerViewMinimal"
     def __init__(self, parent):
@@ -107,12 +106,14 @@ class FirepowerViewMinimal(StatsView):
         test = 1
         #wx.PostEvent(self.mainFrame, EffectiveHpToggled(effective=self.stEHPs.GetLabel() == "HP"))
 
+
         viewName = self.name
 
         menu = ContextMenu.getMenu(None, (viewName,))
-        if menu is not None:
-            contentPanel.PopupMenu(menu)
+        #if menu is not None:
+        #    contentPanel.PopupMenu(menu)
 
+        test = 1
         #event.Skip()
 
     def refreshPanel(self, fit):
