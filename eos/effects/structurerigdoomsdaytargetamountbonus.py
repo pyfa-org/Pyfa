1ae641b9d8762ce7df1a4bd3aafa472f245bc441
Ebag333 <ebagola@gmail.com>
2016-09-19 13:28:44 -0700

    Fix two XL Rig effect files, disable one

--- eos/effects/structurerigdoomsdaytargetamountbonus.py
+++ eos/effects/structurerigdoomsdaytargetamountbonus.py
@@ -1,5 +1,7 @@
+'''
 type = "passive"
 def handler(fit, src, context):
-    fit.modules.filteredItemIncrease(lambda mod: mod.item.group.name == "Structure Doomsday Weapon",
-                                     "lightningWeaponTargetAmount", src.getModifiedItemAttr("structureRigDoomsdayTargetAmountBonus"),
-                                     stackingPenalties=True)
+    fit.modules.filteredItemIncrease(lambda mod: mod.item.group.name == "Structure Doomsday Weapon", "lightningWeaponTargetAmount", src.getModifiedItemAttr("structureRigDoomsdayTargetAmountBonus"), stackingPenalties=True)
+    # This is all wrong.  We need to modify the module attached to the ship (citadel) and add the bonus from the rig.
+    # So Rig -> Ship -> Module
+'''
