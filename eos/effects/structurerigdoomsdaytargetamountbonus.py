325ff92839f4af256888a57b8e0e3e525c13a5cf
Ebag333 <ebagola@gmail.com>
2016-09-22 15:08:04 -0700

    Removed old code

--- eos/effects/structurerigdoomsdaytargetamountbonus.py
+++ eos/effects/structurerigdoomsdaytargetamountbonus.py
@@ -1,11 +1,3 @@
-'''
-type = "passive"
-def handler(fit, src, context):
-    fit.modules.filteredItemIncrease(lambda mod: mod.item.group.name == "Structure Doomsday Weapon", "lightningWeaponTargetAmount", src.getModifiedItemAttr("structureRigDoomsdayTargetAmountBonus"), stackingPenalties=True)
-    # This is all wrong.  We need to modify the module attached to the ship (citadel) and add the bonus from the rig.
-    # So Rig -> Ship -> Module
-'''
-
 type = "passive"
 def handler(fit, src, context):
     for module in fit.modules:
