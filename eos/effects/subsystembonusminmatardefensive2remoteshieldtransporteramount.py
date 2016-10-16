3e4b748952d38e13416a9a08f3ba055df53e113c
Ebag333 <ebagola@gmail.com>
2016-10-16 12:47:50 -0700

    Subsystems calculate after projection calcs
    Moved the 4 remote rep subsystem effect bonuses to calculate early in
    the engine.  This correctly calculates these bonuses for effects,
    previously these effect bonuses wouldn't be calculated until after
    projection calcs ran.

--- eos/effects/subsystembonusminmatardefensive2remoteshieldtransporteramount.py
+++ eos/effects/subsystembonusminmatardefensive2remoteshieldtransporteramount.py
@@ -3,6 +3,7 @@
 # Used by:
 # Subsystem: Loki Defensive - Adaptive Shielding
 type = "passive"
+runTime = "early"
 def handler(fit, module, context):
     fit.modules.filteredItemBoost(lambda mod: mod.item.requiresSkill("Shield Emission Systems"),
                                   "shieldBonus", module.getModifiedItemAttr("subsystemBonusMinmatarDefensive2"), skill="Minmatar Defensive Systems")
