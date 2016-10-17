eb9b034d65f705b97bdb939dcb7f80fc2b1149b9
Ebag333 <ebagola@gmail.com>
2016-10-16 20:52:09 -0700

    Fixed maths with more than 1 drone
    Also tightened up code a bit. Does nothing now if there isn't a
    miningDroneAmountPercent

--- eos/effects/mining.py
+++ eos/effects/mining.py
@@ -7,9 +7,6 @@ type = "passive"
 def handler(fit, container, context):
     miningDroneAmountPercent = container.getModifiedItemAttr("miningDroneAmountPercent")
     if (miningDroneAmountPercent is None) or (miningDroneAmountPercent == 0):
-        miningDroneAmountPercent = 1
+        pass
     else:
-        miningDroneAmountPercent = miningDroneAmountPercent/100
-
-    fit.drones.filteredItemMultiply(lambda drone: drone.item.group.name == "Mining Drone",
-                                 "miningAmount", miningDroneAmountPercent)
+        container.multiplyItemAttr("miningAmount", miningDroneAmountPercent/100)
