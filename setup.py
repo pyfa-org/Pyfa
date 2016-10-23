0f7fe5b5a9904ad77885c369bc10d877eb802560
Ebag333 <ebagola@gmail.com>
2016-10-23 13:17:16 -0700

    More requirements. Check wxversion

--- setup.py
+++ setup.py
@@ -2,11 +2,16 @@ from pip.req import parse_requirements
 from setuptools import setup, find_packages
 # from eos import version
 import config
+import wxversion
 
+allowed_wxversion=['3.0.2']
+try:
+    wxversion.select(allowed_wxversion)
+except wxversion.VersionError:
+    print ("wx version failed detection")
 
 parsed_reqs = parse_requirements('requirements.txt', session=False)
 install_requires = [str(ir.req) for ir in parsed_reqs]
-
 
 setup(
     name='Pyfa',
