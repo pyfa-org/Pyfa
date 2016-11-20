ea15112ad6e3510d3411634550f972f6abcdfa66
Ebag333 <ebagola@gmail.com>
2016-11-20 15:50:33 -0800

    Industrial Core for Rorq

--- eos/effects/industrialcoreeffect2.py
+++ eos/effects/industrialcoreeffect2.py
@@ -4,21 +4,97 @@
 # Module: Industrial Core I
 type = "active"
 runTime = "early"
-def handler(fit, module, context):
+
+
+def handler(fit, src, context):
     fit.extraAttributes["siege"] = True
-    fit.ship.boostItemAttr("maxVelocity", module.getModifiedItemAttr("speedFactor"))
-    fit.ship.multiplyItemAttr("mass", module.getModifiedItemAttr("siegeMassMultiplier"))
+    fit.ship.boostItemAttr("maxVelocity", src.getModifiedItemAttr("speedFactor"))
+    fit.ship.multiplyItemAttr("mass", src.getModifiedItemAttr("siegeMassMultiplier"))
+    fit.ship.multiplyItemAttr("scanResolution", src.getModifiedItemAttr("scanResolutionMultiplier"))
 
+    #  Remote Shield Repper Bonuses
+    fit.modules.filteredItemBoost(lambda mod: mod.item.requiresSkill("Capital Shield Emission Systems"),
+                                  "duration",
+                                  src.getModifiedItemAttr("industrialCoreRemoteLogisticsDurationBonus"),
+                                  )
+    fit.modules.filteredItemBoost(lambda mod: mod.item.requiresSkill("Capital Shield Emission Systems"),
+                                  "capacitorNeed",
+                                  src.getModifiedItemAttr("industrialCoreRemoteLogisticsRangeBonus"),
+                                  )
 
-    fit.modules.filteredItemIncrease(lambda mod: mod.item.group.name in
-                                                 (
-                                                     "Command Burst",
-                                                 ) and
-                                                 mod.item.requiresSkill in
-                                                 (
-                                                     "Shield Command",
-                                                 ),
-                                     "warfareBuff4Value",
-                                     src.getModifiedItemAttr("shipBonusORECapital3"),
-                                     skill="Capital Industrial Ships",
-                                     )
+    #  Local Shield Repper Bonuses
+    fit.modules.filteredItemBoost(lambda mod: mod.item.requiresSkill("Capital Shield Operation"),
+                                  "duration",
+                                  src.getModifiedItemAttr("industrialCoreLocalLogisticsDurationBonus"),
+                                  )
+    fit.modules.filteredItemBoost(lambda mod: mod.item.requiresSkill("Capital Shield Operation"),
+                                  "shieldBonus",
+                                  src.getModifiedItemAttr("industrialCoreLocalLogisticsAmountBonus"),
+                                  )
+
+    # Mining Burst Bonuses
+    fit.modules.filteredItemBoost(lambda mod: mod.item.requiresSkill("Mining Foreman"),
+                                  "warfareBuff1Value",
+                                  src.getModifiedItemAttr("industrialCoreBonusMiningBurstStrength"),
+                                  )
+
+    fit.modules.filteredItemBoost(lambda mod: mod.item.requiresSkill("Mining Foreman"),
+                                  "warfareBuff2Value",
+                                  src.getModifiedItemAttr("industrialCoreBonusMiningBurstStrength"),
+                                  )
+
+    fit.modules.filteredItemBoost(lambda mod: mod.item.requiresSkill("Mining Foreman"),
+                                  "warfareBuff3Value",
+                                  src.getModifiedItemAttr("industrialCoreBonusMiningBurstStrength"),
+                                  )
+
+    fit.modules.filteredItemBoost(lambda mod: mod.item.requiresSkill("Mining Foreman"),
+                                  "warfareBuff4Value",
+                                  src.getModifiedItemAttr("industrialCoreBonusMiningBurstStrength"),
+                                  )
+
+    #  Command Burst Range Bonus
+    fit.modules.filteredItemBoost(lambda mod: mod.item.group.name in
+                                              (
+                                                  "Command Burst",
+                                              ),
+                                  "buffDuration",
+                                  src.getModifiedItemAttr("industrialCoreBonusCommandBurstRange"),
+                                  )
+
+    # Drone Bonuses
+    fit.drones.filteredItemBoost(lambda drone: drone.item.requiresSkill("Ice Harvesting Drone Operation"),
+                                 "duration",
+                                 src.getModifiedItemAttr("industrialCoreBonusDroneIceHarvesting"),
+                                 )
+    fit.drones.filteredItemBoost(lambda drone: drone.item.requiresSkill("Mining Drone Operation"),
+                                 "miningAmount",
+                                 src.getModifiedItemAttr("industrialCoreBonusDroneMining"),
+                                 )
+    fit.drones.filteredItemBoost(lambda drone: drone.item.requiresSkill("Drones"),
+                                 "maxVelocity",
+                                 src.getModifiedItemAttr("industrialCoreBonusDroneVelocity"),
+                                 )
+    fit.drones.filteredItemBoost(lambda drone: drone.item.requiresSkill("Ice Harvesting Drone Operation"),
+                                 "duration",
+                                 src.getModifiedItemAttr("industrialCoreBonusDroneIceHarvesting"),
+                                 )
+    fit.drones.filteredItemBoost(lambda drone: drone.item.requiresSkill("Drones"),
+                                 "damageMultiplier",
+                                 src.getModifiedItemAttr("industrialCoreBonusDroneDamageHP"),
+                                 )
+    fit.drones.filteredItemBoost(lambda drone: drone.item.requiresSkill("Drones"),
+                                 "shieldCapacity",
+                                 src.getModifiedItemAttr("industrialCoreBonusDroneDamageHP"),
+                                 )
+    fit.drones.filteredItemBoost(lambda drone: drone.item.requiresSkill("Drones"),
+                                 "armorHP",
+                                 src.getModifiedItemAttr("industrialCoreBonusDroneDamageHP"),
+                                 )
+    fit.drones.filteredItemBoost(lambda drone: drone.item.requiresSkill("Drones"),
+                                 "hp",
+                                 src.getModifiedItemAttr("industrialCoreBonusDroneDamageHP"),
+                                 )
+
+    #  Todo: remote impedance (no reps, etc)
+2
