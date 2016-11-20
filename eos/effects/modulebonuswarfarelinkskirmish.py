8ad639a122c57d8b779187983235c3c1eaaf3524
blitzman <ryan.xgamer99@gmail.com>
2016-11-20 01:54:45 -0500

    Add titan command effects

--- eos/effects/modulebonuswarfarelinkskirmish.py
+++
@@ -1,6 +0,0 @@
-
-type = "passive"
-def handler(fit, src, context):
-    print "Inside the MODULE"
-    #print "1 ID/value", src.getModifiedChargeAttr("warfareBuff1ID"), src.getModifiedChargeAttr("warfareBuff1Value")
-
