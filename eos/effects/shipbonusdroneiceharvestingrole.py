6500e6d4670053d9a97704e640d1bff8431dee3a
blitzman <ryan.xgamer99@gmail.com>
2016-11-23 20:01:56 -0500

    Fix some drone effects (#840)

--- eos/effects/shipbonusdroneiceharvestingrole.py
+++ eos/effects/shipbonusdroneiceharvestingrole.py
@@ -4,4 +4,4 @@
 # Ship: Orca
 type = "passive"
 def handler(fit, src, context):
-    fit.modules.filteredChargeBoost(lambda mod: mod.charge.requiresSkill("Ice Harvesting Drone Operation"), "duration", src.getModifiedItemAttr("roleBonusDroneIceHarvestingSpeed"))
+    fit.drones.filteredItemBoost(lambda mod: mod.item.requiresSkill("Ice Harvesting Drone Operation"), "duration", src.getModifiedItemAttr("roleBonusDroneIceHarvestingSpeed"))
