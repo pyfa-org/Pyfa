ab5f3488e7afd8f9c78cd2b8e6ff3e8bfe0c7a2d
Indiction <indiction.eve@gmail.com>
2016-12-04 19:56:42 +0100

    Adding Fighter Support CREST

--- service/port.py
+++ service/port.py
@@ -46,6 +46,7 @@ INV_FLAGS = {
 
 INV_FLAG_CARGOBAY = 5
 INV_FLAG_DRONEBAY = 87
+INV_FLAG_FIGHTER = 158
 
 class Port(object):
     """Service which houses all import/export format functions"""
@@ -131,6 +132,15 @@ class Port(object):
             item['type']['name'] = ''
             fit['items'].append(item)
 
+        for fighter in ofit.fighters:
+            item = nested_dict()
+            item['flag'] = INV_FLAG_FIGHTER
+            item['quantity'] = fighter.amountActive
+            item['type']['href'] = "%sinventory/types/%d/"%(eve._authed_endpoint, fighter.item.ID)
+            item['type']['id'] = fighter.item.ID
+            item['type']['name'] = fighter.item.name
+            fit['items'].append(item)
+
         return json.dumps(fit)
 
     @classmethod
@@ -196,6 +206,9 @@ class Port(object):
                     c = Cargo(item)
                     c.amount = module['quantity']
                     f.cargo.append(c)
+                elif module['flag'] == INV_FLAG_FIGHTER:
+                    fighter = Fighter(item)
+                    f.fighters.append(fighter)
                 else:
                     try:
                         m = Module(item)
