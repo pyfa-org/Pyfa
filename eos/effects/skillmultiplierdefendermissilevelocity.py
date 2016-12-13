cd7d25e4e15534f6fc86340e1a357ed3c83e0ac2
Ebag333 <ebagola@gmail.com>
2016-12-13 14:28:43 -0800

    Add skill bonus

---
+++ eos/effects/skillmultiplierdefendermissilevelocity.py
@@ -0,0 +1,6 @@
+type = "passive"
+
+
+def handler(fit, skill, context):
+    fit.modules.filteredChargeBoost(lambda mod: mod.charge.requiresSkill("Defender Missiles"),
+                                    "maxVelocity", skill.getModifiedItemAttr("missileVelocityBonus") * skill.level)
