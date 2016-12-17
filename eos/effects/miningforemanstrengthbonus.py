3d9dae0d529aea50d44eac51150b96889abfbce9
blitzman <ryan.xgamer99@gmail.com>
2016-12-16 20:50:36 -0500

    fuuu

--- eos/effects/miningforemanstrengthbonus.py
+++ eos/effects/miningforemanstrengthbonus.py
@@ -7,5 +7,5 @@ def handler(fit, src, context):
     lvl = src.level
     fit.modules.filteredItemBoost(lambda mod: mod.item.requiresSkill("Mining Foreman"), "warfareBuff4Value", src.getModifiedItemAttr("commandStrengthBonus") * lvl)
     fit.modules.filteredItemBoost(lambda mod: mod.item.requiresSkill("Mining Foreman"), "warfareBuff3Value", src.getModifiedItemAttr("commandStrengthBonus") * lvl)
-    fit.modules.filteredItemBoost(la    mbda mod: mod.item.requiresSkill("Mining Foreman"), "warfareBuff2Value", src.getModifiedItemAttr("commandStrengthBonus") * lvl)
+    fit.modules.filteredItemBoost(lambda mod: mod.item.requiresSkill("Mining Foreman"), "warfareBuff2Value", src.getModifiedItemAttr("commandStrengthBonus") * lvl)
     fit.modules.filteredItemBoost(lambda mod: mod.item.requiresSkill("Mining Foreman"), "warfareBuff1Value", src.getModifiedItemAttr("commandStrengthBonus") * lvl)
