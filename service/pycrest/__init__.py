bb38b695af1f1005697bd99e441b66e8f587b460
Ebag333 <ebagola@gmail.com>
2017-01-30 10:05:37 -0800

    Remove blackholing crest loging

--- service/pycrest/__init__.py
+++ service/pycrest/__init__.py
@@ -1,12 +1,1 @@
-import logging
-
-
-class NullHandler(logging.Handler):
-    def emit(self, record):
-        pass
-
-
-logger = logging.getLogger('pycrest')
-logger.addHandler(NullHandler())
-
 version = "0.0.1"
