43caa5c60d25b845dc73ce29134ca2c14988cbee
Ebag333 <ebagola@gmail.com>
2017-02-02 17:40:52 -0800

    Remove connection strings
    Not necessary since defined at a higher level and not used.
    
    (cherry picked from commit 8e1fefc)

--- eos/config.py
+++ eos/config.py
@@ -5,11 +5,6 @@ debug = False
 gamedataCache = True
 saveddataCache = True
 gamedata_version = ""
-gamedata_connectionstring = 'sqlite:///' + unicode(realpath(join(dirname(abspath(__file__)), "..", "eve.db")),
-                                                   sys.getfilesystemencoding())
-saveddata_connectionstring = 'sqlite:///' + unicode(
-    realpath(join(dirname(abspath(__file__)), "..", "saveddata", "saveddata.db")), sys.getfilesystemencoding())
-
 
 # Autodetect path, only change if the autodetection bugs out.
 path = dirname(unicode(__file__, sys.getfilesystemencoding()))
