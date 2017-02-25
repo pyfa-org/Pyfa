083c5d17222e62bf490154f4c66f68704429fb51
blitzman <ryan.xgamer99@gmail.com>
2017-02-25 13:23:21 -0500

    Bump stable

--- config.py
+++ config.py
@@ -19,9 +19,9 @@ saveInRoot = False
 
 # Version data
 version = "1.27.1"
-tag = "git"
+tag = "Stable"
 expansionName = "YC119.2"
-expansionVersion = "1.2"
+expansionVersion = "1.4"
 evemonMinVersion = "4081"
 
 pyfaPath = None
