1c265e08e7c3799994ac55a2a0b9d6d57dd5c7ff
blitzman <ryan.xgamer99@gmail.com>
2017-02-27 01:22:20 -0500

    Forgot a comma >.<

--- eos/effects/elitebonuscommandshipskirmishcs3.py
+++ eos/effects/elitebonuscommandshipskirmishcs3.py
@@ -7,5 +7,5 @@ type = "passive"
 
 def handler(fit, src, context):
     attrs = ["warfareBuff1Value", "warfareBuff2Value", "warfareBuff3Value", "warfareBuff4Value", "buffDuration"]
-    fit.modules.filteredItemBoost(lambda mod: mod.item.requiresSkill("Skirmish Command"), attrs
+    fit.modules.filteredItemBoost(lambda mod: mod.item.requiresSkill("Skirmish Command"), attrs,
                                   src.getModifiedItemAttr("eliteBonusCommandShips3"), skill="Command Ships")
