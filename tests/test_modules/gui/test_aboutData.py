38a7a50d1cacd927a4eaf783b142eec32888c7a1
Ebag333 <ebagola@gmail.com>
2017-03-22 19:36:45 -0700

    move helpers functions out
    (cherry picked from commit 2047d3f)

--- tests/test_modules/gui/test_aboutData.py
+++ tests/test_modules/gui/test_aboutData.py
@@ -1,5 +1,13 @@
-from gui.aboutData import versionString, licenses, developers, credits, description
+import os
+import sys
 
+'''
+script_dir = os.path.dirname(os.path.abspath(__file__))
+# Add root folder to python paths
+sys.path.append(os.path.realpath(os.path.join(script_dir, '..', '..')))
+'''
+
+from gui.aboutData import versionString, licenses, developers, credits, description
 
 def test_aboutData():
     """
