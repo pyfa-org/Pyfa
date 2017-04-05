2b41c25280de7d46c53a55adefa24ecf4c9ca6b1
Ebag333 <ebagola@gmail.com>
2017-04-04 23:54:22 -0700

    withBoosters

--- service/fit.py
+++ service/fit.py
@@ -1062,6 +1062,6 @@ class Fit(object):
             fit.factorReload = self.serviceFittingOptions["useGlobalForceReload"]
         fit.clear()
 
-        fit.calculateModifiedAttributes(withBoosters=False)
+        fit.calculateModifiedAttributes(withBoosters=withBoosters)
 
         pyfalog.info("=" * 10 + "recalc time: " + str(time() - start_time) + "=" * 10)
