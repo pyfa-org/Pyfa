e2061a25c2748de2405d481f1015fb2658436e8b
blitzmann <holmes.ryan.90@gmail.com>
2017-04-16 20:54:26 -0400

    Update effects for logi

--- eos/effects/shipbonusrole5remotearmorrepairpowergridbonus.py
+++ eos/effects/shipbonusrole5remotearmorrepairpowergridbonus.py
@@ -7,4 +7,4 @@ type = "passive"
 
 def handler(fit, src, context):
     fit.modules.filteredItemBoost(lambda mod: mod.item.requiresSkill("Remote Armor Repair Systems"), "power",
-                                  src.getModifiedItemAttr("remoteArmorPowerNeedBonus"))
+                                  src.getModifiedItemAttr("shipBonusRole5"))
