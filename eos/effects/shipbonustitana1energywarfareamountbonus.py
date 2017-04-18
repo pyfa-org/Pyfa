9d1c77f5b7a782cf217c975e615c5ae7c3c15052
blitzmann <holmes.ryan.90@gmail.com>
2017-04-17 21:19:51 -0400

    Damn you effects...

--- eos/effects/shipbonustitana1energywarfareamountbonus.py
+++ eos/effects/shipbonustitana1energywarfareamountbonus.py
@@ -4,6 +4,5 @@
 # Ship: Molok
 type = "passive"
 def handler(fit, src, context):
-    lvl = src.level
-    fit.modules.filteredItemBoost(lambda mod: mod.item.group.name == "Energy Nosferatu", "powerTransferAmount", src.getModifiedItemAttr("shipBonusTitanA1") * lvl, skill="Amarr Titan")
-    fit.modules.filteredItemBoost(lambda mod: mod.item.group.name == "Energy Neutralizer", "energyNeutralizerAmount", src.getModifiedItemAttr("shipBonusTitanA1") * lvl, skill="Amarr Titan")
+    fit.modules.filteredItemBoost(lambda mod: mod.item.group.name == "Energy Nosferatu", "powerTransferAmount", src.getModifiedItemAttr("shipBonusTitanA1"), skill="Amarr Titan")
+    fit.modules.filteredItemBoost(lambda mod: mod.item.group.name == "Energy Neutralizer", "energyNeutralizerAmount", src.getModifiedItemAttr("shipBonusTitanA1"), skill="Amarr Titan")
