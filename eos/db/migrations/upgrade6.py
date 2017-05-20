77ce125c9c409dd207cd06a8b97cdfd3707d1c8d
blitzmann <holmes.ryan.90@gmail.com>
2017-05-20 02:36:37 -0400

    Update some migrations to consider new schema

--- eos/db/migrations/upgrade6.py
+++ eos/db/migrations/upgrade6.py
@@ -7,5 +7,5 @@ Overwrites damage profile 0 to reset bad uniform values (bad values set with bug
 
 def upgrade(saveddata_engine):
     saveddata_engine.execute('DELETE FROM damagePatterns WHERE name LIKE ? OR ID LIKE ?', ("Uniform", "1"))
-    saveddata_engine.execute('INSERT INTO damagePatterns VALUES (?, ?, ?, ?, ?, ?, ?)',
-                             (1, "Uniform", 25, 25, 25, 25, None))
+    saveddata_engine.execute('INSERT INTO damagePatterns VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
+                             (1, "Uniform", 25, 25, 25, 25, None, None, None))
