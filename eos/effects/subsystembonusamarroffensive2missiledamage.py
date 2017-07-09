78b273455db1b32ee34282a8b4f1fa16394a810f
blitzmann <holmes.ryan.90@gmail.com>
2017-07-09 14:44:57 -0400

    Fix up some effects

--- eos/effects/subsystembonusamarroffensive2missiledamage.py
+++ eos/effects/subsystembonusamarroffensive2missiledamage.py
@@ -4,7 +4,7 @@
 # Subsystem: Legion Offensive - Assault Optimization
 type = "passive"
 def handler(fit, src, context):
-    fit.modules.filteredItemBoost(lambda mod: mod.item.requiresSkill("Missile Launcher Operation"), "explosiveDamage", src.getModifiedItemAttr("subsystemBonusAmarrOffensive2"), skill="Amarr Offensive Systems")
-    fit.modules.filteredItemBoost(lambda mod: mod.item.requiresSkill("Missile Launcher Operation"), "kineticDamage", src.getModifiedItemAttr("subsystemBonusAmarrOffensive2"), skill="Amarr Offensive Systems")
-    fit.modules.filteredItemBoost(lambda mod: mod.item.requiresSkill("Missile Launcher Operation"), "emDamage", src.getModifiedItemAttr("subsystemBonusAmarrOffensive2"), skill="Amarr Offensive Systems")
-    fit.modules.filteredItemBoost(lambda mod: mod.item.requiresSkill("Missile Launcher Operation"), "thermalDamage", src.getModifiedItemAttr("subsystemBonusAmarrOffensive2"), skill="Amarr Offensive Systems")
+    fit.modules.filteredChargeBoost(lambda mod: mod.item.requiresSkill("Missile Launcher Operation"), "explosiveDamage", src.getModifiedItemAttr("subsystemBonusAmarrOffensive2"), skill="Amarr Offensive Systems")
+    fit.modules.filteredChargeBoost(lambda mod: mod.item.requiresSkill("Missile Launcher Operation"), "kineticDamage", src.getModifiedItemAttr("subsystemBonusAmarrOffensive2"), skill="Amarr Offensive Systems")
+    fit.modules.filteredChargeBoost(lambda mod: mod.item.requiresSkill("Missile Launcher Operation"), "emDamage", src.getModifiedItemAttr("subsystemBonusAmarrOffensive2"), skill="Amarr Offensive Systems")
+    fit.modules.filteredChargeBoost(lambda mod: mod.item.requiresSkill("Missile Launcher Operation"), "thermalDamage", src.getModifiedItemAttr("subsystemBonusAmarrOffensive2"), skill="Amarr Offensive Systems")
--- eos/effects/subsystemmrsbfittingreduction.py
+++ eos/effects/subsystemmrsbfittingreduction.py
@@ -5,7 +5,9 @@
 # Subsystem: Tengu Offensive - Support Processor
 type = "passive"
 def handler(fit, src, context):
-    fit.modules.filteredItemBoost(lambda mod: mod.item.requiresSkill("Shield Emission Systems"),
+    fit.modules.filteredItemBoost(lambda mod: mod.item.requiresSkill("Shield Emission Systems") and
+                                              mod.getModifiedItemAttr('mediumRemoteRepFittingMultiplier', 0) == 1,
                                   "cpu", src.getModifiedItemAttr("subsystemMRSBFittingReduction"))
-    fit.modules.filteredItemBoost(lambda mod: mod.item.requiresSkill("Shield Emission Systems"),
+    fit.modules.filteredItemBoost(lambda mod: mod.item.requiresSkill("Shield Emission Systems") and
+                                              mod.getModifiedItemAttr('mediumRemoteRepFittingMultiplier', 0) == 1,
                                   "power", src.getModifiedItemAttr("subsystemMRSBFittingReduction"))
