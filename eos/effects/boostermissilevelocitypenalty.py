a01284ff1c52d883dd882b566665f75dc6f9f52b
burnsypet <aburnoffice@gmail.com>
2017-09-14 21:53:53 +0100

    Convert booster attribute to value before applying
    Changes to boostermissilevelocitypenalty.py:
    Get booster attribute value using booster.getModifiedItemAttr()
    before attempting to apply it to avoid type error

--- eos/effects/boostermissilevelocitypenalty.py
+++ eos/effects/boostermissilevelocitypenalty.py
@@ -14,4 +14,4 @@ attr = "boosterMissileVelocityPenalty"
 
 def handler(fit, booster, context):
     fit.modules.filteredChargeBoost(lambda mod: mod.charge.requiresSkill("Missile Launcher Operation"),
-                                    "maxVelocity", attr)
+                                    "maxVelocity", booster.getModifiedItemAttr(attr))
