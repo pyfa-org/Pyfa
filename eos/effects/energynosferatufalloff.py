4355f35eefce4144c396ed7162f7b39b08e52ab7
blitzmann <holmes.ryan.90@gmail.com>
2017-10-29 15:07:05 -0400

    Revert "Stop applying neut resistance twice" and instead remove the resistance application from `addDrain()`
    This reverts commit 7b32fe08ace93814144d14a2b04238bd4570f2b0.

--- eos/effects/energynosferatufalloff.py
+++ eos/effects/energynosferatufalloff.py
@@ -12,6 +12,9 @@ def handler(fit, src, context, **kwargs):
     amount = src.getModifiedItemAttr("powerTransferAmount")
     time = src.getModifiedItemAttr("duration")
 
+    if 'effect' in kwargs:
+        amount *= ModifiedAttributeDict.getResistance(fit, kwargs['effect'])
+
     if "projected" in context:
         fit.addDrain(src, time, amount, 0)
     elif "module" in context:
