cacf286176bf321c8d4408551f2bc89889f9cdf5
oaao <oliver.antoniu@gmail.com>
2017-11-02 04:41:57 -0400

    [fix] eos - remove Komodo thermalDamage double-dipping

--- eos/effects/shipbonustitang2alldamagebonus.py
+++ eos/effects/shipbonustitang2alldamagebonus.py
@@ -6,21 +6,15 @@ type = "passive"
 
 
 def handler(fit, src, context):
-    fit.modules.filteredChargeBoost(lambda mod: mod.item.requiresSkill("Torpedoes"), "thermalDamage",
-                                  src.getModifiedItemAttr("shipBonusTitanG2"), skill="Gallente Titan")
     fit.modules.filteredChargeBoost(lambda mod: mod.item.requiresSkill("Torpedoes"), "explosiveDamage",
                                   src.getModifiedItemAttr("shipBonusTitanG2"), skill="Gallente Titan")
     fit.modules.filteredChargeBoost(lambda mod: mod.item.requiresSkill("Torpedoes"), "emDamage",
-                                  src.getModifiedItemAttr("shipBonusTitanG2"), skill="Gallente Titan")
-    fit.modules.filteredChargeBoost(lambda mod: mod.item.requiresSkill("XL Torpedoes"), "thermalDamage",
                                   src.getModifiedItemAttr("shipBonusTitanG2"), skill="Gallente Titan")
     fit.modules.filteredChargeBoost(lambda mod: mod.item.requiresSkill("XL Torpedoes"), "emDamage",
                                   src.getModifiedItemAttr("shipBonusTitanG2"), skill="Gallente Titan")
     fit.modules.filteredChargeBoost(lambda mod: mod.item.requiresSkill("XL Torpedoes"), "explosiveDamage",
                                   src.getModifiedItemAttr("shipBonusTitanG2"), skill="Gallente Titan")
     fit.modules.filteredChargeBoost(lambda mod: mod.item.requiresSkill("XL Cruise Missiles"), "emDamage",
-                                  src.getModifiedItemAttr("shipBonusTitanG2"), skill="Gallente Titan")
-    fit.modules.filteredChargeBoost(lambda mod: mod.item.requiresSkill("XL Cruise Missiles"), "thermalDamage",
                                   src.getModifiedItemAttr("shipBonusTitanG2"), skill="Gallente Titan")
     fit.modules.filteredChargeBoost(lambda mod: mod.item.requiresSkill("XL Cruise Missiles"), "explosiveDamage",
                                   src.getModifiedItemAttr("shipBonusTitanG2"), skill="Gallente Titan")
