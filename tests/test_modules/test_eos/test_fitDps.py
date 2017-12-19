39cb89631988ea68039e358e02d0a837b5e2932d
taleden <taleden0@gmail.com>
2017-12-19 16:29:11 -0600

    added test for applied DPS

---
+++ tests/test_modules/test_eos/test_fitDps.py
@@ -0,0 +1,46 @@
+# Add root folder to python paths
+# This must be done on every test in order to pass in Travis
+import math
+import os
+import sys
+script_dir = os.path.dirname(os.path.abspath(__file__))
+script_dir = os.path.realpath(os.path.join(script_dir, '..', '..', '..'))
+print script_dir
+sys.path.append(script_dir)
+
+# noinspection PyPackageRequirements
+from _development.helpers import DBInMemory as DB, Gamedata, Saveddata
+from _development.helpers_fits import RifterFit
+from eos.graph.fitDps import FitDpsGraph
+
+def test_fit_dps(DB, Saveddata, RifterFit):
+    """
+    Tests applied damage
+    """
+
+    # set pilot
+    char0 = Saveddata['Character'].getAll0()
+    RifterFit.character = char0
+
+    # add a turret
+    mod = Saveddata['Module'](DB['db'].getItem("280mm Howitzer Artillery II"))
+    mod.state = Saveddata['State'].ACTIVE
+    ammo = DB['gamedata_session'].query(Gamedata['Item']).filter(Gamedata['Item'].name == "EMP S").first()
+    mod.charge = ammo
+    RifterFit.modules.append(mod)
+
+    # update values
+    RifterFit.clear()
+    RifterFit.calculateModifiedAttributes()
+
+    # calculate applied DPS
+    graph = FitDpsGraph(RifterFit)
+    data = graph.defaults.copy()
+    data['emRes'] = 10.0
+    data['distance'] = 7.0
+    data['signatureRadius'] = 50.0
+    data['tgtAngle'] = 90.0
+    data['tgtSpeed'] = 100.0
+    dps = graph.calcDps(data)
+
+    assert abs(dps - 8.6) < 0.05
