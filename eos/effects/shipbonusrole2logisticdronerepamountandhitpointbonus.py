0c971a127c380e40ee480727764134d728c50c1f
Ryan Holmes <holmes.ryan.90@gmail.com>
2018-01-02 01:43:55 -0500

    Fix loggerhead drone bonus (#1348) (#1358)

--- eos/effects/shipbonusrole2logisticdronerepamountandhitpointbonus.py
+++ eos/effects/shipbonusrole2logisticdronerepamountandhitpointbonus.py
@@ -6,15 +6,15 @@ type = "passive"
 
 
 def handler(fit, src, context):
-    fit.modules.filteredChargeBoost(lambda mod: mod.charge.requiresSkill("Repair Drone Operation"),
+    fit.drones.filteredItemBoost(lambda mod: mod.item.requiresSkill("Repair Drone Operation"),
                                     "structureDamageAmount", src.getModifiedItemAttr("shipBonusRole2"))
-    fit.modules.filteredChargeBoost(lambda mod: mod.charge.requiresSkill("Repair Drone Operation"),
+    fit.drones.filteredItemBoost(lambda mod: mod.item.requiresSkill("Repair Drone Operation"),
                                     "shieldBonus", src.getModifiedItemAttr("shipBonusRole2"))
-    fit.modules.filteredChargeBoost(lambda mod: mod.charge.requiresSkill("Repair Drone Operation"),
+    fit.drones.filteredItemBoost(lambda mod: mod.item.requiresSkill("Repair Drone Operation"),
                                     "armorDamageAmount", src.getModifiedItemAttr("shipBonusRole2"))
-    fit.modules.filteredChargeBoost(lambda mod: mod.charge.requiresSkill("Repair Drone Operation"),
+    fit.drones.filteredItemBoost(lambda mod: mod.item.requiresSkill("Repair Drone Operation"),
                                     "armorHP", src.getModifiedItemAttr("shipBonusRole2"))
-    fit.modules.filteredChargeBoost(lambda mod: mod.charge.requiresSkill("Repair Drone Operation"),
+    fit.drones.filteredItemBoost(lambda mod: mod.item.requiresSkill("Repair Drone Operation"),
                                     "shieldCapacity", src.getModifiedItemAttr("shipBonusRole2"))
-    fit.modules.filteredChargeBoost(lambda mod: mod.charge.requiresSkill("Repair Drone Operation"),
+    fit.drones.filteredItemBoost(lambda mod: mod.item.requiresSkill("Repair Drone Operation"),
                                     "hp", src.getModifiedItemAttr("shipBonusRole2"))
