55ad52919e21615dddd598469464e744a126407f
blitzmann <holmes.ryan.90@gmail.com>
2018-02-15 20:09:50 -0500

    Add missing Ishkur effect (drone tracking)

---
+++ eos/effects/shipbonusdronetrackingelitegunship2.py
@@ -0,0 +1,3 @@
+type = "passive"
+def handler(fit, src, context):
+    fit.drones.filteredItemBoost(lambda mod: mod.item.requiresSkill("Drones"), "trackingSpeed", src.getModifiedItemAttr("eliteBonusGunship2"), skill="Assault Frigates")
