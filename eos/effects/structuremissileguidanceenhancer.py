0f7dd7cc0ce97f78868d558b5ee4c0db66ce0692
Neugeniko <neugeniko@gmail.com>
2018-03-10 00:50:58 +1100

    Correct the structure guidance enhancers to apply bonuses to relevant charges.
    (cherry picked from commit 5c20ee7ade37fa2df17c640f4a016d8178fbe92b)

--- eos/effects/structuremissileguidanceenhancer.py
+++ eos/effects/structuremissileguidanceenhancer.py
@@ -1,14 +1,15 @@
 # Not used by any item
-type = "active"
+type = "passive"
 
 
 def handler(fit, container, context):
+    missileGroups = ("Structure Anti-Capital Missile", "Structure Anti-Subcapital Missile")
     for srcAttr, tgtAttr in (
             ("aoeCloudSizeBonus", "aoeCloudSize"),
             ("aoeVelocityBonus", "aoeVelocity"),
             ("missileVelocityBonus", "maxVelocity"),
             ("explosionDelayBonus", "explosionDelay"),
     ):
-        fit.modules.filteredChargeBoost(lambda mod: mod.charge.requiresSkill("Missile Launcher Operation"),
+        fit.modules.filteredChargeBoost(lambda mod: mod.charge.group.name in missileGroups,
                                         tgtAttr, container.getModifiedItemAttr(srcAttr),
                                         stackingPenalties=True)
