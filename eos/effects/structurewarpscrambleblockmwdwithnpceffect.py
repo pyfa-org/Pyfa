1efcc8d3ef58a9bc2466eb9ce35913902abe5e57
blitzmann <holmes.ryan.90@gmail.com>
2018-03-10 12:09:51 -0500

    style

--- eos/effects/structurewarpscrambleblockmwdwithnpceffect.py
+++ eos/effects/structurewarpscrambleblockmwdwithnpceffect.py
@@ -11,7 +11,8 @@ def handler(fit, module, context):
         fit.ship.increaseItemAttr("warpScrambleStatus", module.getModifiedItemAttr("warpScrambleStrength"))
         if module.charge is not None and module.charge.ID == 47336:
             for mod in fit.modules:
-                if not mod.isEmpty and mod.item.requiresSkill("High Speed Maneuvering") and mod.state > State.ONLINE:
-                    mod.state = State.ONLINE
-                if not mod.isEmpty and mod.item.requiresSkill("Micro Jump Drive Operation") and mod.state > State.ONLINE:
+                if not mod.isEmpty and mod.state > State.ONLINE and (
+                        mod.item.requiresSkill("High Speed Maneuvering")
+                        or mod.item.requiresSkill("Micro Jump Drive Operation")
+                ):
                     mod.state = State.ONLINE
