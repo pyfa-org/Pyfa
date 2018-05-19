9e1260c52d3d861c42a6e7bf52d1c4870b2004fc
Alexey Min <alexey.min@gmail.com>
2018-05-19 12:36:31 +0300

    Enable popup menu with "Copy" command in item traits pane
    Copy selection to clipboard

--- gui/builtinItemStatsViews/itemTraits.py
+++ gui/builtinItemStatsViews/itemTraits.py
@@ -13,5 +13,24 @@ class ItemTraits(wx.Panel):
         self.traits = wx.html.HtmlWindow(self)
         self.traits.SetPage(item.traits.traitText)
 
+        self.traits.Bind(wx.EVT_CONTEXT_MENU, self.showPopupMenu)
+
         mainSizer.Add(self.traits, 1, wx.ALL | wx.EXPAND, 0)
         self.Layout()
+
+        self.popupMenu = wx.Menu()
+        copyItem = wx.MenuItem(self.popupMenu, 1, 'Copy')
+        self.popupMenu.Append(copyItem)
+        self.popupMenu.Bind(wx.EVT_MENU, self.menuClickHandler, copyItem)
+
+    def showPopupMenu(self, event):
+        self.PopupMenu(self.popupMenu)
+
+    def menuClickHandler(self, event):
+        selectedMenuItem = event.GetId()
+        if selectedMenuItem == 1:  # Copy was chosen
+            selectedText = self.traits.SelectionToText()
+            if len(selectedText) > 0:
+                if wx.TheClipboard.Open():
+                    wx.TheClipboard.SetData(wx.TextDataObject(selectedText))
+                    wx.TheClipboard.Close()
