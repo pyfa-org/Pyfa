61abcf3018f2a313f09a654ad2c96a59598d980b
DarkPhoenix <phoenix@mail.ru>
2018-11-22 16:29:10 +0300

    Apply all components of abyssal pvp weather

--- eos/effects/weatherdarkness.py
+++ eos/effects/weatherdarkness.py
@@ -10,10 +10,9 @@ type = ("projected", "passive", "gang")
 
 
 def handler(fit, beacon, context, **kwargs):
-    for x in range(1, 3):
+    for x in range(1, 5):
         if beacon.getModifiedItemAttr("warfareBuff{}ID".format(x)):
             value = beacon.getModifiedItemAttr("warfareBuff{}Value".format(x))
             id = beacon.getModifiedItemAttr("warfareBuff{}ID".format(x))
-
             if id:
                 fit.addCommandBonus(id, value, beacon, kwargs['effect'], 'early')
