3f0fc3c44bb02b14fb661d515489f8d16542a495
blitzmann <holmes.ryan.90@gmail.com>
2018-12-03 21:22:59 -0500

    added effect

---
+++ eos/effects/missileaoevelocitybonus.py
@@ -0,0 +1,3 @@
+type = "passive"
+def handler(fit, src, context):
+    fit.modules.filteredItemBoost(lambda mod: mod.item.requiresSkill("Missile Launcher Operation"), "aoeVelocity", src.getModifiedItemAttr("aoeVelocityBonus"), stackingPenalties=True, skill="Target Navigation Prediction")
