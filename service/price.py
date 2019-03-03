b03860e765a88619576222f3f22552ce4ad670f1
DarkPhoenix <phoenix@mail.ru>
2019-03-03 19:57:59 +0300

    Add implant, booster and cargo support to fit price optimization

--- service/price.py
+++ service/price.py
@@ -234,7 +234,7 @@ class Price:
 
         def cb(replacementMap):
             changes = False
-            for container in (fit.modules, fit.drones, fit.fighters):
+            for container in (fit.modules, fit.drones, fit.fighters, fit.implants, fit.boosters, fit.cargo):
                 for obj in container:
                     charge = getattr(obj, 'charge', None)
                     if charge is not None and charge in replacementMap:
