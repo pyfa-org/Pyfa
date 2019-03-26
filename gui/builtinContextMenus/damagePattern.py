8fed78d5962b7554d2d9447375375c1ecf5afd61
Ryan Holmes <holmes.ryan.90@gmail.com>
2019-03-26 09:44:43 -0400

    Add a few conditionals in case menu is spawned without a fit being loaded

--- gui/builtinContextMenus/damagePattern.py
+++ gui/builtinContextMenus/damagePattern.py
@@ -70,12 +70,12 @@ class DamagePattern(ContextMenu):
         # determine active pattern
         sFit = Fit.getInstance()
         fitID = self.mainFrame.getActiveFit()
-        f = sFit.getFit(fitID)
-        dp = f.damagePattern
-
-        if dp == pattern:
-            bitmap = BitmapLoader.getBitmap("state_active_small", "gui")
-            menuItem.SetBitmap(bitmap)
+        fit = sFit.getFit(fitID)
+        if fit:
+            dp = fit.damagePattern
+            if dp == pattern:
+                bitmap = BitmapLoader.getBitmap("state_active_small", "gui")
+                menuItem.SetBitmap(bitmap)
         return menuItem
 
     def getSubMenu(self, context, selection, rootMenu, i, pitem):
@@ -88,9 +88,9 @@ class DamagePattern(ContextMenu):
             id = pitem.GetId()
             self.patternIds[id] = self.singles[i]
             rootMenu.Bind(wx.EVT_MENU, self.handlePatternSwitch, pitem)
-            if self.patternIds[id] == self.fit.damagePattern:
-                bitmap = BitmapLoader.getBitmap("state_active_small", "gui")
-                pitem.SetBitmap(bitmap)
+            if self.fit and self.patternIds[id] == self.fit.damagePattern:
+                    bitmap = BitmapLoader.getBitmap("state_active_small", "gui")
+                    pitem.SetBitmap(bitmap)
             return False
 
         sub = wx.Menu()
