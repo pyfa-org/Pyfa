2800637a90d85df65696c158bd06cda77fdcfe85
DarkPhoenix <phoenix@mail.ru>
2019-04-12 19:51:18 +0300

    Rework cargo fit commands

--- gui/fitCommands/calc/fitRemoveCargo.py
+++ gui/fitCommands/calc/fitRemoveCargo.py
@@ -1,47 +1,40 @@
 import wx
-import eos.db
 from logbook import Logger
+
+import eos.db
+from service.fit import Fit
+
+
 pyfalog = Logger(__name__)
 
 
 class FitRemoveCargoCommand(wx.Command):
-    """"
-    Fitting command that sets the amount for an item within the cargo.
 
-    from sFit.removeCargo
-    """
-    def __init__(self, fitID, itemID, amount=1, stack=False):
-        wx.Command.__init__(self, True, "Cargo remove")
+    def __init__(self, fitID, itemID, amount=1):
+        wx.Command.__init__(self, True, 'Remove Cargo')
         self.fitID = fitID
         self.itemID = itemID
-        self.stack = stack  # remove entire stack
-        self.amount = amount  # remove x amount. If this goes over amount, removes stack
-        self.old_amount = None
+        self.amount = amount  # Pass infinity if you want to remove stack
+        self.savedRemovedAmount = None
 
     def Do(self):
-        pyfalog.debug("Removing cargo {0} (x{1}) for fit {2}", self.itemID, self.amount, self.fitID)
-        fit = eos.db.getFit(self.fitID)
+        pyfalog.debug('Doing removal of cargo {} x{} from fit {}'.format(self.itemID, self.amount, self.fitID))
+        fit = Fit.getInstance().getFit(self.fitID)
         cargo = next((x for x in fit.cargo if x.itemID == self.itemID), None)
-
         if cargo is None:
             return False
 
-        self.old_amount = cargo.amount
+        self.savedRemovedAmount = min(cargo.amount, self.amount)
 
-        if self.amount >= cargo.amount:
-            self.stack = True  # set to full stack, this allows easier logic in the Undo function
-
-        if self.stack or self.amount >= cargo.amount:
+        cargo.amount -= self.savedRemovedAmount
+        if cargo.amount <= 0:
             fit.cargo.remove(cargo)
-            eos.db.commit()
-            return True
 
-        cargo.amount -= self.amount
         eos.db.commit()
         return True
 
     def Undo(self):
-        from gui.fitCommands.calc.fitAddCargo import FitAddCargoCommand  # Avoid circular import
-        cmd = FitAddCargoCommand(self.fitID, self.itemID, self.old_amount, True)
-        cmd.Do()
-        return True
+        pyfalog.debug('Undoing removal of cargo {} x{} from fit {}'.format(self.itemID, self.amount, self.fitID))
+        from gui.fitCommands.calc.fitAddCargo import FitAddCargoCommand
+        cmd = FitAddCargoCommand(fitID=self.fitID, itemID=self.itemID, amount=self.savedRemovedAmount)
+        return cmd.Do()
