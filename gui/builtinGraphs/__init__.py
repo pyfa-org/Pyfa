51294f6cbc1e87d15379cf880130b916637f0c94
DarkPhoenix <phoenix@mail.ru>
2019-05-13 06:46:25 +0300

    Change the way graphs are imported

--- gui/builtinGraphs/__init__.py
+++ gui/builtinGraphs/__init__.py
@@ -1,12 +1,13 @@
-__all__ = [
-    'fitDpsRange',
-    'fitDpsTime',
-    'fitDmgTime',
-    'fitShieldRegenAmount',
-    'fitShieldAmountTime',
-    'fitCapRegenAmount',
-    'fitCapAmountTime',
-    'fitSpeedTime',
-    'fitDistanceTime',
-    'fitWarpTimeDistance'
-]
+# noinspection PyUnresolvedReferences
+from gui.builtinGraphs import (  # noqa: E402,F401
+    fitDpsRange,
+    fitDpsTime,
+    fitDmgTime,
+    fitShieldRegenAmount,
+    fitShieldAmountTime,
+    fitCapRegenAmount,
+    fitCapAmountTime,
+    fitSpeedTime,
+    fitDistanceTime,
+    fitWarpTimeDistance
+)
