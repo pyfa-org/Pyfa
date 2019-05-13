d17e6d08d8324bebae18fa562c35b7be338117a7
DarkPhoenix <phoenix@mail.ru>
2019-05-13 10:03:19 +0300

    Fix definite integral calculation for distance traveled graph

--- eos/graph/fitDistanceTime.py
+++ eos/graph/fitDistanceTime.py
@@ -22,5 +22,7 @@ class FitDistanceTimeGraph(Graph):
         agility = self.fit.ship.getModifiedItemAttr('agility')
         # Definite integral of:
         # https://wiki.eveuniversity.org/Acceleration#Mathematics_and_formulae
-        distance = maxSpeed * time + (maxSpeed * agility * mass * math.exp((-time * 1000000) / (agility * mass)) / 1000000)
+        distance_t = maxSpeed * time + (maxSpeed * agility * mass * math.exp((-time * 1000000) / (agility * mass)) / 1000000)
+        distance_0 = maxSpeed * 0 + (maxSpeed * agility * mass * math.exp((-0 * 1000000) / (agility * mass)) / 1000000)
+        distance = distance_t - distance_0
         return distance
