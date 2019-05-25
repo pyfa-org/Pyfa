45452ca680984831c530282f8efa1c4119640972
DarkPhoenix <phoenix@mail.ru>
2019-05-25 22:04:17 +0300

    Add rename mappings for faction trig guns

---
+++ service/conversions/releaseInvasion.py
@@ -0,0 +1,10 @@
+"""
+Conversion pack for Invasion release
+"""
+
+CONVERSIONS = {
+    # Renamed items
+    "Light Veles Entropic Disintegrator": "Veles Light Entropic Disintegrator",
+    "Heavy Veles Entropic Disintegrator": "Veles Heavy Entropic Disintegrator",
+    "Supratidal Veles Entropic Disintegrator": "Veles Supratidal Entropic Disintegrator",
+}
