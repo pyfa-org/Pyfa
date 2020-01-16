ad9905a5e04244dc76e77350330be6ea9e8a1565
DarkPhoenix <phoenix@mail.ru>
2020-01-16 15:46:30 +0300

    Add shield booster tieritice conversions

---
+++ service/conversions/releaseJan2020.py
@@ -0,0 +1,14 @@
+"""
+Conversion pack for Januray 2020 release
+"""
+
+CONVERSIONS = {
+    'Small C5-L Emergency Shield Overload I': 'Small C5-L Compact Shield Booster',
+    'Small Clarity Ward Booster I': 'Small Clarity Ward Enduring Shield Booster',
+    'Medium Clarity Ward Booster I': 'Medium Clarity Ward Enduring Shield Booster',
+    'Medium C5-L Emergency Shield Overload I': 'Medium C5-L Compact Shield Booster',
+    'Large Clarity Ward Booster I': 'Large Clarity Ward Enduring Shield Booster',
+    'Large C5-L Emergency Shield Overload I': 'Large C5-L Compact Shield Booster',
+    'X-Large Clarity Ward Booster I': 'X-Large Clarity Ward Enduring Shield Booster',
+    'X-Large C5-L Emergency Shield Overload I': 'X-Large C5-L Compact Shield Booster'
+}
