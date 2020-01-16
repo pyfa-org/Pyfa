0ee86f4836807ebefc0c3bfd98d363e6f5217560
DarkPhoenix <phoenix@mail.ru>
2020-01-16 15:34:56 +0300

    Add slaves to renames dictionary

--- service/conversions/releaseAug2019.py
+++ service/conversions/releaseAug2019.py
@@ -4,5 +4,23 @@ Conversion pack for August 2019 release
 
 CONVERSIONS = {
     # Renamed items
-    "Gravid 1MN Afterburner MutaPlasmid": "Gravid 1MN Afterburner Mutaplasmid",
+    'Gravid 1MN Afterburner MutaPlasmid': 'Gravid 1MN Afterburner Mutaplasmid',
+    'Low-grade Slave Alpha': 'Low-grade Amulet Alpha',
+    'Low-grade Slave Beta': 'Low-grade Amulet Beta',
+    'Low-grade Slave Gamma': 'Low-grade Amulet Gamma',
+    'Low-grade Slave Delta': 'Low-grade Amulet Delta',
+    'Low-grade Slave Epsilon': 'Low-grade Amulet Epsilon',
+    'Low-grade Slave Omega': 'Low-grade Amulet Omega',
+    'Mid-grade Slave Alpha': 'Mid-grade Amulet Alpha',
+    'Mid-grade Slave Beta': 'Mid-grade Amulet Beta',
+    'Mid-grade Slave Gamma': 'Mid-grade Amulet Gamma',
+    'Mid-grade Slave Delta': 'Mid-grade Amulet Delta',
+    'Mid-grade Slave Epsilon': 'Mid-grade Amulet Epsilon',
+    'Mid-grade Slave Omega': 'Mid-grade Amulet Omega',
+    'High-grade Slave Alpha': 'High-grade Amulet Alpha',
+    'High-grade Slave Beta': 'High-grade Amulet Beta',
+    'High-grade Slave Gamma': 'High-grade Amulet Gamma',
+    'High-grade Slave Delta': 'High-grade Amulet Delta',
+    'High-grade Slave Epsilon': 'High-grade Amulet Epsilon',
+    'High-grade Slave Omega': 'High-grade Amulet Omega'
 }
