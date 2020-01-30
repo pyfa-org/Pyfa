a7a4be133c0eeb54bab6a821e725a8ff37f22591
DarkPhoenix <phoenix@mail.ru>
2020-01-30 13:04:49 +0300

    Bump pyinstaller version once again

--- scripts/setup-osx.sh
+++ scripts/setup-osx.sh
@@ -5,4 +5,4 @@ sudo python3 -m ensurepip --upgrade
 # A manual check that the correct version of Python is running.
 python3 --version
 python3 -m pip install -r requirements.txt
-python3 -m pip install PyInstaller==3.3
+python3 -m pip install PyInstaller==3.6
