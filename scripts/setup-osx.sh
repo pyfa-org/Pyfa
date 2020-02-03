fc9337df67a6762b47c5108cfecfcd0d8bb07498
DarkPhoenix <phoenix@mail.ru>
2020-02-03 13:37:27 +0300

    Add more info prints to mac os packaging scripts

--- scripts/setup-osx.sh
+++ scripts/setup-osx.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-echo "Checking python version"
+echo "Python version:"
 python3 --version
-echo "Upgrading pip"
+echo "Upgrading pip..."
 python3 -m pip install --upgrade pip
-echo "Installing app requirements"
+echo "Installing app requirements..."
 python3 -m pip install -r requirements.txt
-echo "Installing packaging tools"
+echo "Installing packaging tools..."
 python3 -m pip install PyInstaller==3.6
