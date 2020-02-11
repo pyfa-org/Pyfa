3b26bfc0e9f19268d515ef542304355ead50b4df
DarkPhoenix <phoenix@mail.ru>
2020-02-11 17:02:00 +0300

    Add missing entries to migration

--- eos/db/migrations/upgrade36.py
+++ eos/db/migrations/upgrade36.py
@@ -1,10 +1,58 @@
 """
 Migration 36
 
-- Capacitor Transfer Tiericide
+- Shield Booster, Armor Repairer and Capacitor Transfer tiericide
 """
 
 CONVERSIONS = {
+    6441: (  # Small Clarity Ward Enduring Shield Booster
+        6443,  # Small Converse Deflection Catalyzer
+    ),
+    6437: (  # Small C5-L Compact Shield Booster
+        6439,  # Small Neutron Saturation Injector I
+    ),
+    10868: (  # Medium Clarity Ward Enduring Shield Booster
+        10870,  # Medium Converse Deflection Catalyzer
+    ),
+    10872: (  # Medium C5-L Compact Shield Booster
+        10866,  # Medium Neutron Saturation Injector I
+    ),
+    10876: (  # Large Clarity Ward Enduring Shield Booster
+        10878,  # Large Converse Deflection Catalyzer
+    ),
+    10880: (  # Large C5-L Compact Shield Booster
+        10874,  # Large Neutron Saturation Injector I
+    ),
+    10884: (  # X-Large Clarity Ward Enduring Shield Booster
+        10886,  # X-Large Converse Deflection Catalyzer
+    ),
+    10888: (  # X-Large C5-L Compact Shield Booster
+        10882,  # X-Large Neutron Saturation Injector I
+    ),
+    4533: (  # Small ACM Compact Armor Repairer
+        4531,  # Small Inefficient Armor Repair Unit
+    ),
+    4529: (  # Small I-a Enduring Armor Repairer
+        4535,  # Small Automated Carapace Restoration
+    ),
+    4573: (  # Medium ACM Compact Armor Repairer
+        4571,  # Medium Inefficient Armor Repair Unit
+    ),
+    4569: (  # Medium I-a Enduring Armor Repairer
+        4575,  # Medium Automated Carapace Restoration
+    ),
+    22889: (  # 'Meditation' Medium Armor Repairer I
+        4579,  # Medium Nano Armor Repair Unit I
+    ),
+    4613: (  # Large ACM Compact Armor Repairer
+        4611,  # Large Inefficient Armor Repair Unit
+    ),
+    4609: (  # Large I-a Enduring Armor Repairer
+        4615,  # Large Automated Carapace Restoration
+    ),
+    22891: (  # 'Protest' Large Armor Repairer I
+        4621,  # Large 'Reprieve' Vestment Reconstructer I
+    ),
     5093: (  # Small Radiative Scoped Remote Capacitor Transmitter
         5087,  # Small Partial E95a Remote Capacitor Transmitter
     ),
