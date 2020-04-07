7c0bd7aa88b4a27d2173a0bd1aa6169e3f142b8b
DarkPhoenix <phoenix@mail.ru>
2020-04-07 16:41:28 +0300

    Leave only setuptools override

--- scripts/setup-osx.sh
+++ scripts/setup-osx.sh
@@ -4,7 +4,7 @@ python3 --version
 echo "Upgrading pip..."
 python3 -m pip install --upgrade pip
 echo "Installing hardcoded app requirements for debugging..."
-python3 -m pip install matplotlib==3.2.0 setuptools==42.0.2
+python3 -m pip install setuptools==42.0.2
 echo "Installing app requirements..."
 python3 -m pip install -r requirements.txt
 echo "Installing packaging tools..."
