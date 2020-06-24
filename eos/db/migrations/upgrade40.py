433c9555bf620e7ae948f754d81ac4957da6b837
blitzmann <holmes.ryan.90@gmail.com>
2020-06-24 15:18:43 -0400

    Removed the util function - this would actually break older migrations if we added things that didn't exist in future iterations. Each migration should be self-contained

--- eos/db/migrations/upgrade40.py
+++ eos/db/migrations/upgrade40.py
@@ -5,7 +5,6 @@ Imports all item conversions since Migration 28 and runs them against module.bas
 forgotten about since it's been added.
 
 """
-from. utils import convert_modules
 from .upgrade36 import CONVERSIONS as u36
 from .upgrade37 import CONVERSIONS as u37
 from .upgrade38 import CONVERSIONS as u38
@@ -13,4 +12,7 @@ from .upgrade39 import CONVERSIONS as u39
 
 def upgrade(saveddata_engine):
     for conversions in [u36, u37, u38, u39]:
-        convert_modules(saveddata_engine, conversions)
+        for replacement_item, list in conversions.items():
+            for retired_item in list:
+                saveddata_engine.execute('UPDATE "modules" SET "baseItemID" = ? WHERE "baseItemID" = ?',
+                               (replacement_item, retired_item))
