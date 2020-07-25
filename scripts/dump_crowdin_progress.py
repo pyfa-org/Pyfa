fca78a72e4704cd7d76933c744bc97ac759652b1
Ryan Holmes <ryan.holmes@stsaviationgroup.com>
2020-07-25 16:27:42 -0400

    Create translation progress dump script and add to appveryor build (travis soon)

---
+++ scripts/dump_crowdin_progress.py
@@ -0,0 +1,29 @@
+import requests
+import os
+import json
+import wx
+
+key = os.environ.get("CROWDIN_APPI_KEY", "")
+
+params = {
+    'json': '',
+    'key': key
+}
+
+resp = requests.get('https://api.crowdin.com/api/project/pyfa/status', params=params)
+data = resp.json()
+
+for x in data:
+    x['code'] = x['code'].replace('-', '_')
+    lang = wx.Locale.FindLanguageInfo(x['code'])
+    if lang is None:
+        print('Cannot find a match for '+x['code'])
+        continue
+    x['canonical_name'] = lang.CanonicalName
+
+data = {x['canonical_name']: x for x in data}
+
+with open("locale/progress.json", 'w') as file:
+    file.seek(0)
+    file.truncate()
+    json.dump(data, file)
