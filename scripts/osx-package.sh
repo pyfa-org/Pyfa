0991124d5f5c030ce3c99bc0aea6e02469aa06d9
DarkPhoenix <phoenix@mail.ru>
2021-06-08 20:55:10 +0300

    Store package as an appveyor artifact

--- scripts/osx-package.sh
+++ scripts/osx-package.sh
@@ -8,7 +8,4 @@ python3 -m PyInstaller -y --clean --windowed dist_assets/mac/pyfa.spec
 echo "Compressing distributive..."
 cd dist
 zip -r "pyfa-$PYFA_VERSION-mac.zip" pyfa.app
-echo "Uploading distributive to transfer.sh..."
-curl --connect-timeout 30 --max-time 300 --upload-file "pyfa-$PYFA_VERSION-mac.zip" https://transfer.sh/ || echo 'upload to transfer.sh failed'
-echo -e "\n"
 md5 -r "pyfa-$PYFA_VERSION-mac.zip"
