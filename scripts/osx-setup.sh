c3d53f56a997c6379c8f34aec5625b3e1e1d205e
DarkPhoenix <phoenix@mail.ru>
2021-06-08 19:46:43 +0300

    Preinstall pathlib on macOS

--- scripts/osx-setup.sh
+++ scripts/osx-setup.sh
@@ -3,6 +3,8 @@ echo "Python version:"
 python3 --version
 echo "Upgrading pip..."
 python3 -m pip install --upgrade pip
+echo "Installing pathlib2 (wxpython issue workaround)..."
+python3 -m pip install pathlib2
 echo "Installing app requirements..."
 python3 -m pip install -r requirements.txt
 echo "Installing packaging tools..."
