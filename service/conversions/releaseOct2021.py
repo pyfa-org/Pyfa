98fa16d9219e420b2204943f10449a2a70b92cd6
DarkPhoenix <phoenix@mail.ru>
2021-10-28 14:39:57 +0300

    Bump version and add renamed mutaplasmid conversion

--- service/conversions/releaseOct2021.py
+++ service/conversions/releaseOct2021.py
@@ -5,4 +5,5 @@ Conversion pack for October 2021 release
 CONVERSIONS = {
     # Renamed items
     "Quafe Zero": "Quafe Zero Classic",
+    "Exigent Sentry Drone Navigation Mutaplasmid": "Exigent Sentry Drone Precision Mutaplasmid",
 }
