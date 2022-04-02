8679485d584d1b92dffd7032f0821ef80f25f3f0
RMunroe943406 <rmunroe@sutdent.bridgew.edu>
2022-04-02 12:02:21 -0400

    Added damage patterns

--- eos/saveddata/damagePattern.py
+++ eos/saveddata/damagePattern.py
@@ -91,6 +91,12 @@ BUILTINS = OrderedDict([
     (-52, (_c(_t('Bombs')) + _t('Scorch Bomb'), 0, 6400, 0, 0)),
     (-53, (_c(_t('Bombs')) + _t('Concussion Bomb'), 0, 0, 6400, 0)),
     (-54, (_c(_t('Bombs')) + _t('Shrapnel Bomb'), 0, 0, 0, 6400)),
+    (-113, (_c(_t('Condenser Packs')) + _t('BlastShot'), 232, 0, 756, 0)),
+    (-114, (_c(_t('Condenser Packs')) + _t('GalvaSurge'), 756, 0, 231, 0)),
+    (-115, (_c(_t('Condenser Packs')) + _t('MesmerFlux'), 359, 0, 135, 0)),
+    (-116, (_c(_t('Condenser Packs')) + _t('SlamBolt'), 132, 0, 359, 0)),
+    (-117, (_c(_t('Condenser Packs')) + _t('[t2] ElectroPunch'), 595, 0, 591, 0)),
+    (-118, (_c(_t('Condenser Packs')) + _t('[t2] StrikeSnipe'), 325, 0, 321, 0)),
     # Source: ticket #2067 and #2265
     (-55, (_c(_t('NPC')) + _c(_t('Abyssal')) + _t('All'), 126, 427, 218, 230)),
     (-109, (_c(_t('NPC')) + _c(_t('Abyssal')) + _t('Angel'), 450, 72, 80, 398)),
