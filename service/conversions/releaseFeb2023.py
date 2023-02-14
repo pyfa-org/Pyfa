a14210f35674cb3b5a03de0fac29eb86458e15d1
Anton Vorobyov <phoenix@mail.ru>
2023-02-14 19:25:45 +0600

    Add rename mapping

---
+++ service/conversions/releaseFeb2023.py
@@ -0,0 +1,16 @@
+"""
+Conversion pack for February 2023 release
+"""
+
+CONVERSIONS = {
+    # Renamed items
+    "Restrained Interdiction Nullifier": "Enduring Interdiction Nullifier",
+    "Synthetic Hull Conversion Inertia Stabilizers": "Synthetic Hull Conversion Inertial Stabilizers",
+    "Tobias's Modified Torpedo Launcher": "Tobias' Modified Torpedo Launcher",
+    "Vepas's Modified Torpedo Launcher": "Vepas' Modified Torpedo Launcher",
+    "Vepas's Modified Kinetic Shield Hardener": "Vepas' Modified Kinetic Shield Hardener",
+    "Vepas's Modified EM Shield Hardener": "Vepas' Modified EM Shield Hardener",
+    "Vepas's Modified Explosive Shield Hardener": "Vepas' Modified Explosive Shield Hardener",
+    "Vepas's Modified Thermal Shield Hardener": "Vepas' Modified Thermal Shield Hardener",
+    "Vepas's Modified Multispectrum Shield Hardener": "Vepas' Modified Multispectrum Shield Hardener",
+}
