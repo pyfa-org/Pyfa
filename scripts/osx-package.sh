c230480464b91b5de6e3e045a2d5c976103ee868
DarkPhoenix <phoenix@mail.ru>
2023-12-04 23:11:34 +0600

    Do not modify package after it was built

--- scripts/osx-package.sh
+++ scripts/osx-package.sh
@@ -7,7 +7,5 @@ echo "Building distributive..."
 python3 -m PyInstaller -y --clean dist_assets/mac/pyfa.spec
 echo "Compressing distributive..."
 cd dist
-/usr/libexec/PlistBuddy -c "Add :CFBundleVersion string ${PYFA_VERSION}" "pyfa.app/Contents/Info.plist" # Add missing
-/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString ${PYFA_VERSION}" "pyfa.app/Contents/Info.plist" # Modify existing
 zip -r "pyfa-$PYFA_VERSION-mac.zip" pyfa.app
 md5 -r "pyfa-$PYFA_VERSION-mac.zip"
