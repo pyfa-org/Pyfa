56bc5c33760a1a062543be9b7a523fb4f4b39ca0
DarkPhoenix <phoenix@mail.ru>
2024-06-12 16:03:09 +0200

    Fix icons script and add missing renders

--- scripts/icons_update.py
+++ scripts/icons_update.py
@@ -145,6 +145,8 @@ def get_icon_file(res_path, size):
     icon for it. Return as PIL image object down-
     scaled for use in pyfa.
     """
+    res_path = res_path.lower()
+    res_path = res_path.replace('\\', '/')
     res_path = res_path.replace('//', '/')  #1703
     if res_path not in res_index:
         return None
