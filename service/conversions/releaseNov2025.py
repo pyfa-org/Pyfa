6546f32971622e46e8c71238efae6e896dbe2ba3
DarkPhoenix <phoenix@mail.ru>
2025-10-30 02:00:31 +0100

    Add renames

---
+++ service/conversions/releaseNov2025.py
@@ -0,0 +1,5 @@
+CONVERSIONS = {
+    "Survey Scanner I": "Mining Sensor Array I",
+    "Survey Scanner II": "Mining Sensor Array II",
+    "ML-3 Scoped Survey Scanner": "ML-3 Compact Mining Sensor Array",
+}
