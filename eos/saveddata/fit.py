664e5820cb6d3807631e3add5d4a8c587571e61a
Skybladev2 <Skybladev2@users.noreply.github.com>
2026-04-05 22:01:52 +0300

    adujst logs

--- eos/saveddata/fit.py
+++ eos/saveddata/fit.py
@@ -1108,10 +1108,7 @@ class Fit:
         if type == CalcType.COMMAND and targetFit in self.commandFits:
             pyfalog.debug("{} is in the command listing for COMMAND ({}), do not mark self as calculated (recursive)".format(repr(targetFit), repr(self)))
         elif type == CalcType.PROJECTED:
-            # A projected-source pass only applies this fit's remote effects to the target; it is not a full local
-            # calculation of this fit. Marking calculated would make switchFit skip recalc, so bidirectional
-            # projections show wrong stats when switching tabs until the projection is toggled.
-            pass
+            pyfalog.debug("{} is projecting onto {} (PROJECTED), do not mark self as calculated (not a full local calc)".format(repr(self), repr(targetFit)))
         else:
             self.__calculated = True
 
