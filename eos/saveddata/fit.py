a305c4ad78afcc784a2169a3ffe52d4c89f753d1
William Weist <wiweist@gmail.com>
2026-08-28 23:45:08 -0400

    Ignore volume of active fighter for fighterBayUsed

--- eos/saveddata/fit.py
+++ eos/saveddata/fit.py
@@ -1320,7 +1320,8 @@ class Fit:
     def fighterBayUsed(self):
         amount = 0
         for f in self.fighters:
-            amount += f.item.attributes['volume'].value * f.amount
+            if not f.active:
+                amount += f.item.attributes['volume'].value * f.amount
 
         return amount
 
